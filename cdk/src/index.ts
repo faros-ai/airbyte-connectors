b885820b4fac0469905fa85a04d2f60e9f43ff90
Chris Wu <chris@faros.ai>
2021-08-13 10:21:40 -0700

    Add missing export

--- cdk/src/index.ts
+++ cdk/src/index.ts
@@ -2,6 +2,7 @@ export * from './destinations/destination';
 export * from './destinations/destination-runner';
 export * from './logger';
 export * from './protocol';
+export * from './sources/abstract-source';
 export * from './sources/source';
 export * from './sources/source-runner';
 export * from './utils';
