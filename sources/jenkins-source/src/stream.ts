5edb413228376e6d5a3860970b49f1e9c95602cc
Roma-Kyrnis <kirnos007@gmail.com>
2021-08-25 12:15:34 +0300

    add jenkins source

---
+++ sources/jenkins-source/src/stream.ts
@@ -0,0 +1,361 @@
+import {
+  AirbyteConfig,
+  AirbyteLogger,
+  AirbyteStreamBase,
+  StreamKey,
+  SyncMode,
+} from 'cdk';
+import jenkinsClient, {JenkinsPromisifiedAPI} from 'jenkins';
+import {Dictionary} from 'ts-essentials';
+import {URL} from 'url';
+import util from 'util';
+
+const DEFAULT_PAGE_SIZE = 10;
+const FEED_ALL_FIELDS_PATTERN = `name,fullName,url,lastCompletedBuild[number],%s[id,displayName,number,building,result,timestamp,duration,url,actions[lastBuiltRevision[SHA1],remoteUrls]],jobs[*]`;
+const FEED_JOBS_COUNT_PATTERN = 'jobs[name]';
+const FEED_MAX_DEPTH_CALC_PATTERN = 'fullName,jobs[*]';
+const FOLDER_JOB_TYPE = 'com.cloudbees.hudson.plugins.folder.Folder';
+const POTENTIAL_MAX_DEPTH = 10;
+
+export interface JenkinsConfig extends AirbyteConfig {
+  readonly server_url: string;
+  readonly user: string;
+  readonly token: string;
+  readonly depth?: number;
+  readonly pageSize?: number;
+  readonly last100Builds?: boolean;
+}
+
+interface Job {
+  _class: string;
+  fullName: string;
+  name: string;
+  url: string;
+  allBuilds?: Build[];
+  builds?: Build[];
+  lastCompletedBuild: Build | null;
+  jobs?: Job[];
+}
+
+interface Build {
+  _class: string;
+  actions: any[];
+  building: boolean;
+  displayName: string;
+  duration: number;
+  id: string;
+  number: number;
+  result: string;
+  timestamp: number;
+  url: string;
+}
+
+interface BuildJob extends Build {
+  jobFullName: string;
+}
+
+interface JenkinsState {
+  readonly newJobsLastCompletedBuilds: Record<string, number>; // job to build number
+}
+
+function parse(str: string, ...args: any[]): string {
+  let i = 0;
+  return str.replace(/%s/g, () => args[i++]);
+}
+
+export class Jenkins {
+  constructor(
+    private readonly client: any,
+    private readonly logger: AirbyteLogger
+  ) {}
+
+  static async validateClient(
+    config: JenkinsConfig
+  ): Promise<[JenkinsPromisifiedAPI | undefined, string | undefined]> {
+    if (typeof config.server_url !== 'string') {
+      return [undefined, 'server_url: must be a string'];
+    }
+    if (typeof config.user !== 'string') {
+      return [undefined, 'User: must be a string'];
+    }
+    if (typeof config.token !== 'string') {
+      return [undefined, 'Token: must be a string'];
+    }
+
+    let jenkinsUrl;
+    try {
+      jenkinsUrl = new URL(config.server_url);
+    } catch (error) {
+      return [undefined, 'server_url: must be a valid url'];
+    }
+
+    jenkinsUrl.username = config.user;
+    jenkinsUrl.password = config.token;
+
+    const client = jenkinsClient({
+      baseUrl: jenkinsUrl.toString(),
+      crumbIssuer: true,
+      promisify: true,
+    });
+
+    try {
+      await client.info();
+    } catch (error) {
+      return [
+        undefined,
+        'server_url: Please verify your user/token is correct',
+      ];
+    }
+    return [client, undefined];
+  }
+
+  async *syncBuilds(
+    jenkinsCfg: JenkinsConfig,
+    existingState: JenkinsState | null
+  ): AsyncGenerator<BuildJob> {
+    const iter = this.syncJobs(jenkinsCfg, null);
+    for await (const job of iter) {
+      const builds = this.constructBuilds(job, existingState);
+      for (const build of builds) {
+        yield build;
+      }
+    }
+  }
+
+  async *syncJobs(
+    jenkinsCfg: JenkinsConfig,
+    streamSlice: Job | null
+  ): AsyncGenerator<Job> {
+    const pageSize = jenkinsCfg.pageSize || DEFAULT_PAGE_SIZE;
+    const last100Builds = jenkinsCfg.last100Builds ?? false;
+    const depth = jenkinsCfg.depth ?? (await this.calculateMaxJobsDepth());
+    this.logger.debug(parse('Max depth: %s', depth));
+
+    const numRootJobs = await this.countRootJobs();
+    const numPages = Math.ceil(numRootJobs / pageSize);
+    this.logger.debug(
+      parse(
+        'Number of root jobs: %s. Number of pages: %s',
+        numRootJobs,
+        numPages
+      )
+    );
+    for (let i = 0, from, to; i < numRootJobs; i += pageSize) {
+      from = i;
+      to = Math.min(numRootJobs, i + pageSize);
+      const jobs = await this.fetchJobs(depth, last100Builds, from, to);
+      for (const job of jobs) {
+        if (streamSlice && streamSlice.url === job.url) {
+          return undefined;
+        }
+        yield job;
+      }
+    }
+  }
+
+  /*
+  http://localhost:8080/job/job_105_folder/job/job_custom_105/
+  job fullName: job_105_folder/job_custom_105
+  level: 1 (starts with 0)
+  */
+  private async calculateMaxJobsDepth(): Promise<number> {
+    const jobs = await this.client.job.list({
+      depth: POTENTIAL_MAX_DEPTH,
+      tree: this.generateTree(POTENTIAL_MAX_DEPTH, FEED_MAX_DEPTH_CALC_PATTERN),
+    });
+    const allJobs = this.retrieveAllJobs(jobs);
+
+    let maxLevel = 0;
+    for (const job of allJobs) {
+      const level = (job.fullName.match(/\//g) || []).length;
+      if (level > maxLevel) {
+        maxLevel = level;
+      }
+    }
+    return maxLevel;
+  }
+
+  private constructBuilds(
+    job: Job,
+    existingState: JenkinsState | null
+  ): BuildJob[] {
+    const lastBuildNumber =
+      existingState?.newJobsLastCompletedBuilds?.[job.fullName];
+
+    const builds = job.allBuilds ?? job.builds ?? [];
+    if (!builds.length) {
+      this.logger.info(parse("Job '%s' has no builds", job.fullName));
+      return [];
+    }
+    const newCompletedBuilds = lastBuildNumber
+      ? builds.filter(
+          (build: any) => build.number > lastBuildNumber && !build.building
+        )
+      : builds;
+    return newCompletedBuilds.map((b: any) => ({
+      ...b,
+      jobFullName: job.fullName,
+    }));
+  }
+
+  private async countRootJobs(): Promise<number> {
+    const jobs = await this.client.job.list({tree: FEED_JOBS_COUNT_PATTERN});
+    return jobs.length;
+  }
+
+  private async fetchJobs(
+    depth: number,
+    last100Builds: boolean,
+    from: number,
+    to: number
+  ): Promise<Job[]> {
+    const page = `{${from},${to}}`; // `to` parameter is exclusive
+    const builds = last100Builds ? 'builds' : 'allBuilds';
+    try {
+      const rootJobs = await this.client.job.list({
+        // https://www.jenkins.io/doc/book/using/remote-access-api/#RemoteaccessAPI-Depthcontrol
+        depth: depth,
+        tree:
+          this.generateTree(
+            depth,
+            util.format(FEED_ALL_FIELDS_PATTERN, builds)
+          ) + page,
+      });
+
+      return this.retrieveAllJobs(rootJobs);
+    } catch (err) {
+      this.logger.warn(
+        parse(
+          'Failed to fetch jobs in page %s: %s. Skipping page.',
+          page,
+          err.message
+        )
+      );
+      return [];
+    }
+  }
+
+  private generateTree(depth: number, fieldsPattern: string): string {
+    let tree = 'jobs[' + fieldsPattern + ']';
+    for (let i = 0; i < depth; i++) {
+      tree = tree.replace('*', fieldsPattern);
+    }
+
+    return tree;
+  }
+
+  // we need to flatten all jobs to a list from the tree structure
+  private retrieveAllJobs(rootJobs: Job[]): Job[] {
+    const allJobs: Job[] = [];
+    while (rootJobs?.length) {
+      const job = rootJobs.pop();
+      // only jobs with builds are required, skip folders
+      if (!job) {
+        break;
+      }
+      if (job._class === FOLDER_JOB_TYPE) {
+        if (job.jobs) {
+          
+          for (const nestedJob of job.jobs) {
+            nestedJob.name = `${job.name}/${nestedJob.name}`;
+          }
+          rootJobs.push(...job.jobs);
+        }
+      } else {
+        allJobs.push(job);
+      }
+    }
+    return allJobs;
+  }
+}
+
+export class JenkinsBuilds extends AirbyteStreamBase {
+  constructor(readonly config: JenkinsConfig, logger: AirbyteLogger) {
+    super(logger);
+  }
+
+  getJsonSchema(): Dictionary<any, string> {
+    return require('../resources/schemas/builds.json');
+  }
+  get primaryKey(): StreamKey {
+    return ['id', 'jobFullName'];
+  }
+  get cursorField(): string | string[] {
+    return ['number', 'jobFullName'];
+  }
+
+  async *readRecords(
+    syncMode: SyncMode,
+    cursorField?: string[],
+    streamSlice?: BuildJob,
+    streamState?: JenkinsState
+  ): AsyncGenerator<BuildJob, any, any> {
+    const [client, errorMessage] = await Jenkins.validateClient(this.config);
+    if (!client) {
+      this.logger.error(errorMessage || '');
+      return undefined;
+    }
+    const jenkins = new Jenkins(client, this.logger);
+
+    let iter: AsyncGenerator<BuildJob, any, unknown>;
+    if (syncMode === SyncMode.FULL_REFRESH) {
+      iter = jenkins.syncBuilds(this.config, null);
+    } else {
+      iter = jenkins.syncBuilds(this.config, streamState ?? null);
+    }
+    yield* iter;
+  }
+
+  getUpdatedState(
+    currentStreamState: JenkinsState,
+    latestRecord: BuildJob
+  ): JenkinsState {
+    currentStreamState.newJobsLastCompletedBuilds[latestRecord.jobFullName] =
+      Math.max(
+        currentStreamState?.newJobsLastCompletedBuilds[
+          latestRecord.jobFullName
+        ] ?? 0,
+        latestRecord?.number ?? 0
+      );
+    return currentStreamState;
+  }
+}
+
+export class JenkinsJobs extends AirbyteStreamBase {
+  constructor(readonly config: JenkinsConfig, logger: AirbyteLogger) {
+    super(logger);
+  }
+
+  getJsonSchema(): Dictionary<any, string> {
+    return require('../resources/schemas/jobs.json');
+  }
+  get primaryKey(): StreamKey {
+    return 'fullName';
+  }
+  get cursorField(): string | string[] {
+    return ['url'];
+  }
+
+  async *readRecords(
+    syncMode: SyncMode,
+    cursorField?: string[],
+    streamSlice?: Job,
+    streamState?: any
+  ): AsyncGenerator<Job, any, any> {
+    const [client, errorMessage] = await Jenkins.validateClient(this.config);
+    if (!client) {
+      this.logger.error(errorMessage || '');
+      return undefined;
+    }
+
+    const jenkins = new Jenkins(client, this.logger);
+
+    let iter: AsyncGenerator<Job, any, unknown>;
+    if (syncMode === SyncMode.FULL_REFRESH) {
+      iter = jenkins.syncJobs(this.config, null);
+    } else {
+      iter = jenkins.syncJobs(this.config, streamSlice ?? null);
+    }
+    yield* iter;
+  }
+}
---
+++ sources/jenkins-source/test/index.test.ts
@@ -0,0 +1,232 @@
+import {AirbyteLogger, AirbyteSpec, SyncMode} from 'cdk';
+import fs from 'fs-extra';
+import jenkinsClient from 'jenkins';
+import {mocked} from 'ts-jest/utils';
+import {VError} from 'verror';
+
+import {JenkinsSource} from '../src/index';
+
+jest.mock('jenkins');
+
+function readTestResourceFile(fileName: string): any {
+  return JSON.parse(fs.readFileSync(`test_files/${fileName}`, 'utf8'));
+}
+
+describe('index', () => {
+  const logger = new AirbyteLogger();
+
+  test('spec', async () => {
+    const source = new JenkinsSource(logger);
+    expect(source.spec()).resolves.toStrictEqual(
+      new AirbyteSpec(readTestResourceFile('spec.json'))
+    );
+  });
+
+  test('check connection', async () => {
+    mocked(jenkinsClient).mockReturnValueOnce({
+      info: jest.fn().mockResolvedValue({}),
+    } as any);
+    mocked(jenkinsClient).mockReturnValueOnce({
+      info: jest.fn().mockRejectedValue({}),
+    } as any);
+    const source = new JenkinsSource(logger);
+    expect(
+      source.checkConnection({
+        user: '123',
+        token: 'token',
+        server_url: 'http://google.com',
+      })
+    ).resolves.toStrictEqual([true, undefined]);
+    expect(
+      source.checkConnection({
+        user: '123',
+        token: 'token',
+        server_url: 'http://google.com',
+      })
+    ).resolves.toStrictEqual([
+      false,
+      new VError('server_url: Please verify your user/token is correct'),
+    ]);
+    expect(source.checkConnection({} as any)).resolves.toStrictEqual([
+      false,
+      new VError('server_url: must be a string'),
+    ]);
+    expect(
+      source.checkConnection({server_url: '111', user: '', token: ''})
+    ).resolves.toStrictEqual([
+      false,
+      new VError('server_url: must be a valid url'),
+    ]);
+  });
+
+  test('streams - jobs, use incremental sync mode', async () => {
+    mocked(jenkinsClient).mockReturnValue({
+      info: jest.fn().mockResolvedValue({}),
+      job: {
+        list: jest.fn().mockImplementation(async () =>
+          readTestResourceFile('jobs.json')
+        ),
+      },
+    } as any);
+    const source = new JenkinsSource(logger);
+    const [jobStream] = source.streams({
+      server_url: 'http://localhost:8080',
+      user: 'bot',
+      token: 'pass',
+    });
+    const jobsIter = jobStream.readRecords(SyncMode.INCREMENTAL, undefined, {
+      url: 'http://localhost:8080/job/first',
+    });
+    const jobs = [];
+    for await (const job of jobsIter) {
+      jobs.push(job);
+    }
+    expect(jobs).toStrictEqual([]);
+  });
+
+  test('streams - jobs, use full_refresh sync mode', async () => {
+    const fnJobList = jest.fn();
+
+    mocked(jenkinsClient).mockReturnValue({
+      info: jest.fn().mockResolvedValue({}),
+      job: {
+        list: fnJobList.mockImplementation(async () =>
+          readTestResourceFile('jobs.json')
+        ),
+      },
+    } as any);
+    const source = new JenkinsSource(logger);
+    const streams = source.streams({
+      server_url: 'http://localhost:8080',
+      user: 'bot',
+      token: 'pass',
+    });
+    const jobsIter = streams[0].readRecords(SyncMode.FULL_REFRESH);
+    const jobs = [];
+    for await (const job of jobsIter) {
+      jobs.push(job);
+    }
+    expect(fnJobList).toHaveBeenCalledTimes(3);
+    expect(jobs).toStrictEqual([
+      {
+        _class: 'hudson.model.FreeStyleProject',
+        fullName: 'first',
+        name: 'first',
+        url: 'http://localhost:8080/job/first',
+        allBuilds: [
+          {
+            building: false,
+            id: '3',
+            number: 3,
+            url: 'http://localhost:8080/job/first',
+          },
+          {
+            building: false,
+            id: '1',
+            number: 1,
+            url: 'http://localhost:8080/job/first',
+          },
+        ],
+      },
+    ]);
+  });
+
+  test('streams - return undefined if config not correct', async () => {
+    mocked(jenkinsClient).mockReturnValue({
+      info: jest.fn().mockResolvedValue({}),
+    } as any);
+    const source = new JenkinsSource(logger);
+    const [jobStream, buildStream] = source.streams({} as any);
+    const jobIter = jobStream.readRecords(SyncMode.FULL_REFRESH);
+    const buildIter = buildStream.readRecords(SyncMode.FULL_REFRESH);
+    expect((await jobIter.next()).value).toBeUndefined();
+    expect((await buildIter.next()).value).toBeUndefined();
+  });
+
+  test('streams - builds, use incremental sync mode', async () => {
+    mocked(jenkinsClient).mockReturnValue({
+      info: jest.fn().mockResolvedValue({}),
+      job: {
+        list: jest
+          .fn()
+          .mockImplementation(async () => readTestResourceFile('jobs.json')),
+      },
+    } as any);
+    const source = new JenkinsSource(logger);
+    const [, buildStream] = source.streams({
+      server_url: 'http://localhost:8080',
+      user: 'bot',
+      token: 'pass',
+    });
+    const buildState = {
+      newJobsLastCompletedBuilds: {first: 2},
+    };
+    const buildsIter = buildStream.readRecords(
+      SyncMode.INCREMENTAL,
+      undefined,
+      undefined,
+      buildState
+    );
+    const builds = [];
+    expect(
+      buildStream.getUpdatedState(buildState, {jobFullName: 'first', number: 1})
+    ).toStrictEqual({
+      newJobsLastCompletedBuilds: {first: 2},
+    });
+    for await (const build of buildsIter) {
+      builds.push(build);
+    }
+    expect(builds).toStrictEqual([
+      {
+        building: false,
+        id: '3',
+        jobFullName: 'first',
+        number: 3,
+        url: 'http://localhost:8080/job/first',
+      },
+    ]);
+    expect(
+      buildStream.getUpdatedState(buildState, {jobFullName: 'first', number: 3})
+    ).toStrictEqual({
+      newJobsLastCompletedBuilds: {first: 3},
+    });
+  });
+
+  test('streams - builds, use full_refresh sync mode', async () => {
+    mocked(jenkinsClient).mockReturnValue({
+      info: jest.fn().mockResolvedValue({}),
+      job: {
+        list: jest
+          .fn()
+          .mockImplementation(async () => readTestResourceFile('jobs.json')),
+      },
+    } as any);
+    const source = new JenkinsSource(logger);
+    const [, buildStream] = source.streams({
+      server_url: 'http://localhost:8080',
+      user: 'bot',
+      token: 'pass',
+    });
+    const buildsIter = buildStream.readRecords(SyncMode.FULL_REFRESH);
+    const builds = [];
+    for await (const build of buildsIter) {
+      builds.push(build);
+    }
+    expect(builds).toStrictEqual([
+      {
+        building: false,
+        id: '3',
+        jobFullName: 'first',
+        number: 3,
+        url: 'http://localhost:8080/job/first',
+      },
+      {
+        building: false,
+        id: '1',
+        jobFullName: 'first',
+        number: 1,
+        url: 'http://localhost:8080/job/first',
+      },
+    ]);
+  });
+});
