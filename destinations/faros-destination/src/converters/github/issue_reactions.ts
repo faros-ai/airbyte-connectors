ede705ca7d3fd63d08810e0f19a8d9dd39176ee1
Matthew Tovbin <tovbinm@users.noreply.github.com>
2021-09-14 11:28:29 -0700

    Added GitHub converter stubs + added sync modes (#80)

---
+++ destinations/faros-destination/src/converters/github/issue_reactions.ts
@@ -0,0 +1,12 @@
+import {AirbyteRecord} from 'faros-airbyte-cdk';
+
+import {Converter, DestinationModel, DestinationRecord} from '../converter';
+
+export class GithubIssueReactions extends Converter {
+  readonly destinationModels: ReadonlyArray<DestinationModel> = []; // TODO: set destination model
+
+  convert(record: AirbyteRecord): ReadonlyArray<DestinationRecord> {
+    // TODO: convert records
+    return [];
+  }
+}
