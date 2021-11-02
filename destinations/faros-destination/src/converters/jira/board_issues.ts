784360f838a2f67b012e9e424deb036c8b4c68bb
Chris Wu <chris@faros.ai>
2021-11-02 09:57:37 -0700

    Add board_issues

--- destinations/faros-destination/src/converters/jira/board_issues.ts
+++ destinations/faros-destination/src/converters/jira/board_issues.ts
@@ -4,13 +4,25 @@ import {DestinationModel, DestinationRecord, StreamContext} from '../converter';
 import {JiraConverter} from './common';
 
 export class JiraBoardIssues extends JiraConverter {
-  readonly destinationModels: ReadonlyArray<DestinationModel> = []; // TODO: set destination model
+  readonly destinationModels: ReadonlyArray<DestinationModel> = [
+    'tms_TaskBoardRelationship',
+  ];
 
   convert(
     record: AirbyteRecord,
     ctx: StreamContext
   ): ReadonlyArray<DestinationRecord> {
-    // TODO: convert records
-    return [];
+    if (!this.useBoardOwnership(ctx)) return [];
+    const issue = record.record.data;
+    const source = this.streamName.source;
+    return [
+      {
+        model: 'tms_TaskBoardRelationship',
+        record: {
+          task: {uid: issue.key, source},
+          board: {uid: String(issue.boardId), source},
+        },
+      },
+    ];
   }
 }
