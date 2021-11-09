ab167b3e3edc4ac4edfdbb1caf7e8ccc30ae3782
Roma-Kyrnis <kirnos007@gmail.com>
2021-11-09 14:33:32 +0200

    add Harness source and Faros destination's converter

---
+++ destinations/faros-destination/src/converters/harness/common.ts
@@ -0,0 +1,108 @@
+import {AirbyteRecord} from 'faros-airbyte-cdk';
+
+import {Converter} from '../converter';
+
+export interface ComputeApplication {
+  name: string;
+  platform: string;
+}
+
+interface CICDOrganization {
+  uid: string;
+  source: string;
+}
+
+interface CICDPipeline {
+  uid: string;
+  organization: CICDOrganization;
+}
+
+export interface CICDBuild {
+  uid: string;
+  pipeline: CICDPipeline;
+}
+
+interface CICDRepository {
+  uid: string;
+  organization: CICDOrganization;
+}
+
+export interface CICDArtifact {
+  readonly uid: string;
+  readonly repository: CICDRepository;
+}
+
+export interface Execution {
+  uid: string;
+  application: ComputeApplication;
+  startedAt: number;
+  endedAt: number;
+  env: string;
+  status: string;
+  build: CICDBuild;
+  artifact: CICDArtifact;
+}
+
+export interface ExecutionImplementation {
+  application: ComputeApplication;
+  env: string;
+  build?: CICDBuild;
+  artifact?: CICDArtifact;
+}
+
+interface HarnessService {
+  id: string;
+  name?: string;
+  artifactType: string;
+  artifactSources: {
+    name: string;
+  }[];
+}
+
+interface HarnessEnvironment {
+  type: string;
+  name: string;
+}
+
+export interface HarnessExecutionNode {
+  id: string;
+  application: {
+    id: string;
+    name?: string;
+    services?: {
+      nodes: HarnessService[];
+    };
+    environments?: {
+      nodes: HarnessEnvironment[];
+    };
+    tags?: {
+      name: string;
+      value: string;
+    }[];
+  };
+  status: string;
+  createdAt: number;
+  startedAt: number;
+  endedAt: number;
+  artifacts?: {
+    id: string;
+    buildNo: string;
+    artifactSource: {
+      id: string;
+      name: string;
+    };
+  }[];
+  outcomes?: {
+    nodes: {
+      service: HarnessService;
+      environment: HarnessEnvironment;
+    }[];
+  };
+}
+
+/** Harness converter base */
+export abstract class HarnessConverter extends Converter {
+  id(record: AirbyteRecord): any {
+    return record?.record?.data?.id;
+  }
+}
