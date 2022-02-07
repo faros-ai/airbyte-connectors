f62ad843362392cd73bc5a0e5914d826c525e695
Kelvin Tran <huongkstn@gmail.com>
2022-02-07 16:53:56 +0700

    Fix the tests (#186)

--- sources/buildkite-source/test/index.test.ts
+++ sources/buildkite-source/test/index.test.ts
@@ -2,22 +2,18 @@ import {
   AirbyteLogger,
   AirbyteLogLevel,
   AirbyteSpec,
-  redactConfig,
   SyncMode,
 } from 'faros-airbyte-cdk';
 import fs from 'fs-extra';
 import {VError} from 'verror';
 
-import {Buildkite, BuildkiteConfig} from '../src/buildkite/buildkite';
+import {Buildkite} from '../src/buildkite/buildkite';
 import * as sut from '../src/index';
 
 const BuildkiteInstance = Buildkite.instance;
 
 function readTestResourceFile(fileName: string): any {
   return JSON.parse(fs.readFileSync(`test_files/${fileName}`, 'utf8'));
-}
-function readConfig(): BuildkiteConfig {
-  return readTestResourceFile('config.json') as BuildkiteConfig;
 }
 describe('index', () => {
   const logger = new AirbyteLogger(
@@ -37,80 +33,80 @@ describe('index', () => {
       new AirbyteSpec(readTestResourceFile('spec.json'))
     );
   });
-  // test('check connection', async () => {
-  //   const source = new sut.BuildkiteSource(logger);
-  //   await expect(
-  //     source.checkConnection({
-  //       token: readConfig().token,
-  //     })
-  //   ).resolves.toStrictEqual([true, undefined]);
-  // });
+  test('check connection', async () => {
+    const source = new sut.BuildkiteSource(logger);
+    await expect(
+      source.checkConnection({
+        token: '',
+      })
+    ).resolves.toStrictEqual([true, undefined]);
+  });
 
-  // test('check connection - incorrect token', async () => {
-  //   const source = new sut.BuildkiteSource(logger);
-  //   await expect(source.checkConnection({token:''})).resolves.toStrictEqual([
-  //     false,
-  //     new VError('Please verify your token are correct. Error: some error'),
-  //   ]);
-  // });
+  test('check connection - incorrect token', async () => {
+    const source = new sut.BuildkiteSource(logger);
+    await expect(source.checkConnection({token: ''})).resolves.toStrictEqual([
+      false,
+      new VError('Please verify your token are correct. Error: some error'),
+    ]);
+  });
 
-  // test('check connection - incorrect variables', async () => {
-  //   const source = new sut.BuildkiteSource(logger);
-  //   await expect(source.checkConnection({})).resolves.toStrictEqual([
-  //     false,
-  //     new VError('token must be a not empty string'),
-  //   ]);
-  // });
+  test('check connection - incorrect variables', async () => {
+    const source = new sut.BuildkiteSource(logger);
+    await expect(source.checkConnection({})).resolves.toStrictEqual([
+      false,
+      new VError('token must be a not empty string'),
+    ]);
+  });
 
-  // test('streams - organizations, use full_refresh sync mode', async () => {
-  //   const fileName = 'organizations.json';
-  //   const source = new sut.BuildkiteSource(logger);
-  //   const streams = source.streams(readConfig());
-  //   const stream = streams[0];
-  //   const itemIter = stream.readRecords(SyncMode.FULL_REFRESH);
-  //   const items = [];
-  //   for await (const item of itemIter) {
-  //     items.push(item);
-  //   }
-  //   expect(items).toStrictEqual(readTestResourceFile(fileName));
-  // });
+  test('streams - organizations, use full_refresh sync mode', async () => {
+    const fileName = 'organizations.json';
+    const source = new sut.BuildkiteSource(logger);
+    const streams = source.streams({});
+    const stream = streams[0];
+    const itemIter = stream.readRecords(SyncMode.FULL_REFRESH);
+    const items = [];
+    for await (const item of itemIter) {
+      items.push(item);
+    }
+    expect(items).toStrictEqual(readTestResourceFile(fileName));
+  });
 
-  // test('streams - pipelines, use full_refresh sync mode', async () => {
-  //   const fileName = 'pipelines.json';
-  //   const source = new sut.BuildkiteSource(logger);
-  //   const streams = source.streams(readConfig());
-  //   const stream = streams[1];
-  //   const itemIter = stream.readRecords(SyncMode.FULL_REFRESH);
-  //   const items = [];
-  //   for await (const item of itemIter) {
-  //     items.push(item);
-  //   }
-  //   expect(items).toStrictEqual(readTestResourceFile(fileName));
-  // });
+  test('streams - pipelines, use full_refresh sync mode', async () => {
+    const fileName = 'pipelines.json';
+    const source = new sut.BuildkiteSource(logger);
+    const streams = source.streams({});
+    const stream = streams[1];
+    const itemIter = stream.readRecords(SyncMode.FULL_REFRESH);
+    const items = [];
+    for await (const item of itemIter) {
+      items.push(item);
+    }
+    expect(items).toStrictEqual(readTestResourceFile(fileName));
+  });
 
-  // test('streams - builds, use full_refresh sync mode', async () => {
-  //   const fileName = 'builds.json';
-  //   const source = new sut.BuildkiteSource(logger);
-  //   const streams = source.streams(readConfig());
-  //   const stream = streams[2];
-  //   const itemIter = stream.readRecords(SyncMode.FULL_REFRESH);
-  //   const items = [];
-  //   for await (const item of itemIter) {
-  //     items.push(item);
-  //   }
-  //   expect(items).toStrictEqual(readTestResourceFile(fileName));
-  // });
+  test('streams - builds, use full_refresh sync mode', async () => {
+    const fileName = 'builds.json';
+    const source = new sut.BuildkiteSource(logger);
+    const streams = source.streams({});
+    const stream = streams[2];
+    const itemIter = stream.readRecords(SyncMode.FULL_REFRESH);
+    const items = [];
+    for await (const item of itemIter) {
+      items.push(item);
+    }
+    expect(items).toStrictEqual(readTestResourceFile(fileName));
+  });
 
-  // test('streams - jobs, use full_refresh sync mode', async () => {
-  //   const fileName = 'jobs.json';
-  //   const source = new sut.BuildkiteSource(logger);
-  //   const streams = source.streams(readConfig());
-  //   const stream = streams[3];
-  //   const itemIter = stream.readRecords(SyncMode.FULL_REFRESH);
-  //   const items = [];
-  //   for await (const item of itemIter) {
-  //     items.push(item);
-  //   }
-  //   expect(items).toStrictEqual(readTestResourceFile(fileName));
-  // });
+  test('streams - jobs, use full_refresh sync mode', async () => {
+    const fileName = 'jobs.json';
+    const source = new sut.BuildkiteSource(logger);
+    const streams = source.streams({});
+    const stream = streams[3];
+    const itemIter = stream.readRecords(SyncMode.FULL_REFRESH);
+    const items = [];
+    for await (const item of itemIter) {
+      items.push(item);
+    }
+    expect(items).toStrictEqual(readTestResourceFile(fileName));
+  });
 });
