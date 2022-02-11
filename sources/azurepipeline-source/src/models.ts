88aa79602c3a2353f68558f995b1df38b989800d
Kelvin Tran <huongkstn@gmail.com>
2022-02-11 15:10:44 +0700

    Update pipeline models (#240)

--- sources/azurepipeline-source/src/models.ts
+++ sources/azurepipeline-source/src/models.ts
@@ -3,10 +3,20 @@ export interface PipelineResponse {
   value: Pipeline[];
 }
 
+interface PipelineHref {
+  href: string;
+}
+
+interface PipelineLink {
+  self: PipelineHref;
+  web: PipelineHref;
+}
+
 export interface Pipeline {
   id: number;
   revision: number;
   name?: string;
   url?: string;
   folder?: string;
+  _links: PipelineLink[];
 }
