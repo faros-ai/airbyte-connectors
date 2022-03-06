68905d713c039e2719cc657f198cd4e931863039
Kelvin Tran <huongkstn@gmail.com>
2022-03-07 00:32:30 +0700

    Refactoring and remove unused file (#185)

--- sources/shortcut-source/test/index.test.ts
+++ sources/shortcut-source/test/index.test.ts
@@ -12,6 +12,10 @@ import {Shortcut, ShortcutConfig} from '../src/shortcut';
 
 const shortcutInstance = Shortcut.instance;
 
+function readResourceFile(fileName: string): any {
+  return JSON.parse(fs.readFileSync(`resources/${fileName}`, 'utf8'));
+}
+
 function readTestResourceFile(fileName: string): any {
   return JSON.parse(fs.readFileSync(`test_files/${fileName}`, 'utf8'));
 }
@@ -31,7 +35,7 @@ describe('index', () => {
   test('spec', async () => {
     const source = new sut.ShortcutSource(logger);
     await expect(source.spec()).resolves.toStrictEqual(
-      new AirbyteSpec(readTestResourceFile('spec.json'))
+      new AirbyteSpec(readResourceFile('spec.json'))
     );
   });
 
