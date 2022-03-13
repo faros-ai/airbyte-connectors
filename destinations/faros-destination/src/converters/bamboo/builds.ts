d41ac0d84129be8a1888b5d60ed46ca6262f0ff4
Kelvin Tran <huongkstn@gmail.com>
2022-03-13 12:03:22 +0700

    Fix build destination

--- destinations/faros-destination/src/converters/bamboo/builds.ts
+++ destinations/faros-destination/src/converters/bamboo/builds.ts
@@ -1,9 +1,10 @@
 import {AirbyteRecord} from 'faros-airbyte-cdk';
 import {Utils} from 'faros-feeds-sdk/lib';
+import {toLower} from 'lodash';
 
 import {DestinationModel, DestinationRecord, StreamContext} from '../converter';
 import {BambooCommon, BambooConverter} from './common';
-import {Build} from './models';
+import {Build, PipelineKey} from './models';
 
 export class BambooBuilds extends BambooConverter {
   readonly destinationModels: ReadonlyArray<DestinationModel> = ['cicd_Build'];
@@ -13,9 +14,25 @@ export class BambooBuilds extends BambooConverter {
     ctx: StreamContext
   ): Promise<ReadonlyArray<DestinationRecord>> {
     const build = record.record.data as Build;
+
+    const source = this.streamName.source;
+    const baseUrl = this.baseUrl(ctx);
+
+    if (!baseUrl) {
+      return [];
+    }
+    const orgId = new URL(baseUrl).hostname;
+
+    if (!orgId) {
+      return [];
+    }
+
     const buildStartedTime = Utils.toDate(build.buildStartedTime);
     const buildStatus = BambooCommon.convertBuildStatus(build.state);
-
+    const pipelineKey: PipelineKey = {
+      uid: toLower(build.plan.key),
+      organization: {uid: orgId, source},
+    };
     return [
       {
         model: 'cicd_Build',
@@ -27,7 +44,7 @@ export class BambooBuilds extends BambooConverter {
           endedAt: Utils.toDate(build.buildCompletedTime),
           status: buildStatus,
           url: build.link.href,
-          pipeline: build.plan.key,
+          pipeline: pipelineKey,
         },
       },
     ];
