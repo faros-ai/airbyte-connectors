82ab75de0479327e9c038729332a7cc3a36f3796
Matthew Tovbin <tovbinm@gmail.com>
2022-03-15 11:20:31 -0700

    Fix Docker image path for Faros Destination

--- scripts/publish-connector.sh
+++ scripts/publish-connector.sh
@@ -5,22 +5,28 @@ if [ -z "$1" ]; then
 fi
 connector_path=$1
 
-image="farosai/airbyte-$(echo $connector_path | cut -f2 -d'/')"
-echo image name: $image
+org="farosai"
+connector_name="$(echo $connector_path | cut -f2 -d'/')"
+prefix="airbyte-"
+if [[ "$connector_name" = $prefix* ]]; then
+  image="$org/$connector_name"
+else
+  image="$org/$prefix$connector_name"
+fi
+
 latest_tag="$image:latest"
 connector_version=$(jq -r '.version' < ${connector_path}package.json)
 version_tag="$image:$connector_version"
-echo version tag: $version_tag
+echo "Image version tag: $version_tag"
 
 docker manifest inspect $version_tag > /dev/null
-if [ "$?" == 1 ]
-then
+if [ "$?" == 1 ]; then
   docker build . \
     --build-arg path=$connector_path \
     -t $latest_tag \
     -t $version_tag \
     --label "io.airbyte.version=$connector_version" \
     --label "io.airbyte.name=$image"
-  docker push $latest_tag
-  docker push $version_tag
+  # docker push $latest_tag
+  # docker push $version_tag
 fi
