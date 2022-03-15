0ab0827710f888bfc10538f1a753ba87a008839d
Kelvin Tran <huongkstn@gmail.com>
2022-03-16 02:48:32 +0700

    Merge remote-tracking branch 'origin/main' into kelvin/add-bamboo
    # Conflicts:
    #	destinations/airbyte-faros-destination/src/converters/bamboo/builds.ts
    #	destinations/airbyte-faros-destination/src/converters/bamboo/common.ts
    #	destinations/airbyte-faros-destination/src/converters/bamboo/deployments.ts
    #	destinations/airbyte-faros-destination/src/converters/bamboo/models.ts
    #	destinations/airbyte-faros-destination/src/converters/bamboo/plans.ts
    #	destinations/airbyte-faros-destination/test/converters/bamboo.test.ts
    #	destinations/airbyte-faros-destination/test/resources/bamboo/all-streams.log
    #	destinations/airbyte-faros-destination/test/resources/bamboo/catalog.json

---
+++ destinations/airbyte-faros-destination/test/converters/victorops.test.ts
@@ -0,0 +1,92 @@
+import {AirbyteLog, AirbyteLogLevel} from 'faros-airbyte-cdk';
+import fs from 'fs';
+import _ from 'lodash';
+import {getLocal} from 'mockttp';
+import pino from 'pino';
+
+import {CLI, read} from '../cli';
+import {initMockttp, tempConfig} from '../testing-tools';
+import {victoropsAllStreamsLog} from './data';
+
+describe('victorops', () => {
+  const logger = pino({
+    name: 'test',
+    level: process.env.LOG_LEVEL ?? 'info',
+    prettyPrint: {levelFirst: true},
+  });
+  const mockttp = getLocal({debug: false, recordTraffic: false});
+  const catalogPath = 'test/resources/victorops/catalog.json';
+  let configPath: string;
+  const streamNamePrefix = 'mytestsource__victorops__';
+
+  beforeEach(async () => {
+    await initMockttp(mockttp);
+    configPath = await tempConfig(mockttp.url);
+  });
+
+  afterEach(async () => {
+    await mockttp.stop();
+    fs.unlinkSync(configPath);
+  });
+
+  test('process records from all streams', async () => {
+    const cli = await CLI.runWith([
+      'write',
+      '--config',
+      configPath,
+      '--catalog',
+      catalogPath,
+      '--dry-run',
+    ]);
+    cli.stdin.end(victoropsAllStreamsLog, 'utf8');
+
+    const stdout = await read(cli.stdout);
+    logger.debug(stdout);
+
+    const processedByStream = {
+      incidents: 2,
+      teams: 2,
+      users: 1,
+    };
+    const processed = _(processedByStream)
+      .toPairs()
+      .map((v) => [`${streamNamePrefix}${v[0]}`, v[1]])
+      .orderBy(0, 'asc')
+      .fromPairs()
+      .value();
+
+    const writtenByModel = {
+      ims_Incident: 2,
+      ims_IncidentAssignment: 2,
+      ims_IncidentEvent: 4,
+      ims_Team: 2,
+      ims_TeamIncidentAssociation: 2,
+      ims_User: 1,
+    };
+
+    const processedTotal = _(processedByStream).values().sum();
+    const writtenTotal = _(writtenByModel).values().sum();
+    expect(stdout).toMatch(`Processed ${processedTotal} records`);
+    expect(stdout).toMatch(`Would write ${writtenTotal} records`);
+    expect(stdout).toMatch('Errored 0 records');
+    expect(stdout).toMatch('Skipped 0 records');
+    expect(stdout).toMatch(
+      JSON.stringify(
+        AirbyteLog.make(
+          AirbyteLogLevel.INFO,
+          `Processed records by stream: ${JSON.stringify(processed)}`
+        )
+      )
+    );
+    expect(stdout).toMatch(
+      JSON.stringify(
+        AirbyteLog.make(
+          AirbyteLogLevel.INFO,
+          `Would write records by model: ${JSON.stringify(writtenByModel)}`
+        )
+      )
+    );
+    expect(await read(cli.stderr)).toBe('');
+    expect(await cli.wait()).toBe(0);
+  });
+});
