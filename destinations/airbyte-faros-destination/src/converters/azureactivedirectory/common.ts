b964acfd1675c00153898ea96a58030b7ceb8ad6
Kelvin Tran <huongkstn@gmail.com>
2022-03-16 08:56:47 +0700

    Fix test

--- destinations/airbyte-faros-destination/src/converters/azureactivedirectory/common.ts
+++ destinations/airbyte-faros-destination/src/converters/azureactivedirectory/common.ts
@@ -4,7 +4,7 @@ import {Converter} from '../converter';
 
 /** AzureActiveDirectory converter base */
 export abstract class AzureactivedirectoryConverter extends Converter {
-  sorcee = 'AzureActiveDirectory';
+  source = 'AzureActiveDirectory';
   /** Almost every Azure Active Directory record have id property */
   id(record: AirbyteRecord): any {
     return record?.record?.data?.id;
