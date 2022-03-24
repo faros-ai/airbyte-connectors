0829c9f5a455b7bd8f6df88e6ec9a7984da82501
Kelvin Tran <huongkstn@gmail.com>
2022-03-25 01:20:04 +0700

    - Add repository data test - Add tms_Project, tms_TaskBoard for each repository to link issues

--- destinations/airbyte-faros-destination/src/converters/bitbucket/repositories.ts
+++ destinations/airbyte-faros-destination/src/converters/bitbucket/repositories.ts
@@ -14,13 +14,10 @@ export class Repositories extends BitbucketConverter {
   readonly destinationModels: ReadonlyArray<DestinationModel> = [
     'cicd_Repository',
     'vcs_Repository',
+    'tms_Project',
+    'tms_TaskBoard',
+    'tms_TaskBoardProjectRelationship',
   ];
-
-  private readonly workspacesStream = new StreamName('bitbucket', 'workspace');
-
-  override get dependencies(): ReadonlyArray<StreamName> {
-    return [this.workspacesStream];
-  }
 
   async convert(
     record: AirbyteRecord,
@@ -28,48 +25,50 @@ export class Repositories extends BitbucketConverter {
   ): Promise<ReadonlyArray<DestinationRecord>> {
     const source = this.streamName.source;
     const repository = record.record.data as Repository;
-
-    const workspacesStream = this.workspacesStream.asString;
-    const workspacesRecord = ctx.get(
-      workspacesStream,
-      repository.workspace.uuid
-    );
-    const workspace = workspacesRecord?.record?.data as undefined | Workspace;
-    if (!workspace) {
-      return [];
-    }
-
+    const workspace = repository.workspace;
+    const res: DestinationRecord[] = [];
     const description = repository.description?.substring(
       0,
       BitbucketCommon.MAX_DESCRIPTION_LENGTH
     );
-    return [
-      {
-        model: 'cicd_Repository',
-        record: {
-          uid: repository.slug.toLowerCase(),
-          name: repository.fullName,
-          description,
-          url: repository?.links?.htmlUrl,
-          organization: {uid: workspace?.uuid, source},
-        },
+    // Create a TMS Project/Board per repo that we sync
+    res.push(
+      ...BitbucketCommon.tms_ProjectBoard_with_TaskBoard(
+        {uid: repository.name, source},
+        repository.name,
+        repository.description,
+        repository.createdOn,
+        repository.updatedOn
+      )
+    );
+
+    res.push({
+      model: 'cicd_Repository',
+      record: {
+        uid: repository.slug.toLowerCase(),
+        name: repository.fullName,
+        description,
+        url: repository?.links?.htmlUrl,
+        organization: {uid: workspace.uuid, source},
       },
-      {
-        model: 'vcs_Repository',
-        record: {
-          name: repository.slug.toLowerCase(),
-          fullName: repository.fullName,
-          description,
-          private: repository.isPrivate,
-          language: repository.language ?? null,
-          size: BigInt(repository.size),
-          htmlUrl: repository?.links?.htmlUrl,
-          createdAt: Utils.toDate(repository.createdOn),
-          updatedAt: Utils.toDate(repository.updatedOn),
-          mainBranch: repository.mainBranch?.name,
-          organization: {uid: workspace?.uuid, source},
-        },
+    });
+    res.push({
+      model: 'vcs_Repository',
+      record: {
+        name: repository.slug.toLowerCase(),
+        fullName: repository.fullName,
+        description,
+        private: repository.isPrivate,
+        language: repository.language ?? null,
+        size: BigInt(repository.size),
+        htmlUrl: repository?.links?.htmlUrl,
+        createdAt: Utils.toDate(repository.createdOn),
+        updatedAt: Utils.toDate(repository.updatedOn),
+        mainBranch: repository.mainBranch?.name,
+        organization: {uid: workspace.uuid, source},
       },
-    ];
+    });
+
+    return res;
   }
 }
