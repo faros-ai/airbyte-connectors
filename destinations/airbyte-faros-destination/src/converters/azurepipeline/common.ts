9fa1dd7f0d19e1a9147452cc82009b9cb1b58515
huongtn <huongkstn@gmail.com>
2022-04-12 03:37:29 +0700

    Add Azure pipeline connector (#276)
    * Create source and integrate pipeline api (#240)
    
    * Update pipeline models (#240)
    
    * Add build stream (#240)
    
    * Add build specs (#240)
    
    * Fix acceptance test (#240)
    
    * Add release stream (#240)
    
    * Add release stream (#240)
    
    * Add artifacts of build stream (#240)
    
    * Pull jobs from build (#240)
    
    * Add builds, piplelines, releases destinations (#240)
    
    * Parse url repository of build (#240)
    
    * Update build destination (#240)
    
    * Add destination test (#240)
    
    * Fix test destination (#240)
    
    * Add release sample data (#240)
    
    * Add tag to cicd_Artifact (#240)
    
    * Add artifact data (#240)
    
    * Add release data (#240)
    
    * Add vcs_User destination (#240)
    
    * Fix destination test (#240)
    
    * Modify destination model (#240)
    
    * Add runs of pipeline (#240)
    
    * Add cicd_Deployment (#240)
    
    * Correct azurepipeline config functon (#240)
    
    * Update test (#240)
    
    * Correct variable name (#2410)
    
    * Remove unused files (#240)
    
    * Fix after test e2e (#240)
    
    * Remove unused files and update test (#240)
    
    * Remove unused file (#240)
    
    * Fix test
    
    * Remove faros-feeds-sdk from sources
    
    * Remove unused dependencies
    
    * Update for Roman's comment
    
    * Update test
    
    * Rename AzurePipelineConfig
    
    * Add keywords to connector
    
    * Update for Roman's comments
    
    * Remove cursorField
    
    * Change instance  function to sync
    
    * Update release primary key
    
    * Fix acceptance test
    
    * Add incremental mode
    - Incremental mode for builds
    - Incremental mode for releases
    - Add start_date to spec
    
    * Fix test
    
    * Add abnormal_state
    
    * Version
    
    * Update dep versions
    
    * Add the incremental_configured_catalog.json to the test files
    
    * Use cutoff_days
    
    Co-authored-by: Chris Wu <chris@faros.ai>

---
+++ destinations/airbyte-faros-destination/src/converters/azurepipeline/common.ts
@@ -0,0 +1,146 @@
+import {AirbyteRecord} from 'faros-airbyte-cdk';
+import {Utils} from 'faros-feeds-sdk';
+
+import {Converter, StreamContext} from '../converter';
+import {
+  BuildStateCategory,
+  BuildTimeline,
+  DeploymentStatus,
+  DeploymentStatusCategory,
+  JobCategory,
+  JobState,
+  Repository,
+  Timestamps,
+} from './models';
+
+export type ApplicationMapping = Record<
+  string,
+  {name: string; platform?: string}
+>;
+
+interface AzurePipelineConfig {
+  application_mapping?: ApplicationMapping;
+}
+
+/** Azurepipeline converter base */
+export abstract class AzurePipelineConverter extends Converter {
+  source = 'AzurePipeline';
+  /** Almost every Azurepipeline record have id property */
+  id(record: AirbyteRecord): any {
+    return record?.record?.data?.id;
+  }
+
+  getOrganizationFromUrl(url: string): string {
+    return url.split('/')[3];
+  }
+
+  protected azurepipleConfig(ctx: StreamContext): AzurePipelineConfig {
+    return ctx.config?.source_specific_configs?.azurepipeline;
+  }
+
+  protected applicationMapping(ctx: StreamContext): ApplicationMapping {
+    return this.azurepipleConfig(ctx)?.application_mapping ?? {};
+  }
+
+  convertBuildState(state: string | undefined): {
+    category: string;
+    detail: string;
+  } {
+    if (!state) {
+      return {category: BuildStateCategory.Unknown, detail: 'undefined'};
+    }
+    const detail = state.toLowerCase();
+
+    // Read more on Azure pipeline build result:
+    // https://docs.microsoft.com/en-us/rest/api/azure/devops/build/builds/list?view=azure-devops-rest-6.0#buildresult
+    switch (detail) {
+      case 'canceled':
+        return {category: BuildStateCategory.Canceled, detail};
+      case 'failed':
+        return {category: BuildStateCategory.Failed, detail};
+      case 'succeeded':
+      case 'partiallySucceeded':
+        return {category: BuildStateCategory.Success, detail};
+      case 'running':
+        return {category: BuildStateCategory.Running, detail};
+      default:
+        return {category: BuildStateCategory.Custom, detail};
+    }
+  }
+
+  getRepoUrl(repo: Repository): string | undefined {
+    switch (repo.type) {
+      case 'Bitbucket':
+        return `https://bitbucket.org/${repo.id}`;
+      case 'GitHub':
+      case 'GitHubEnterprise':
+        return `https://github.com/${repo.id}`;
+      case 'GitLab':
+        return `https://gitlab.com/${repo.id}`;
+      case 'TfsGit':
+        return repo.url;
+      default:
+        return repo.url;
+    }
+  }
+
+  convertBuildStepTime(buildStep: BuildTimeline): Timestamps {
+    //const type = buildStep.type;
+    const result: Timestamps = {
+      createdAt: Utils.toDate(buildStep.startTime),
+      startedAt: Utils.toDate(buildStep.startTime),
+      endedAt: Utils.toDate(buildStep.finishTime),
+    };
+    return result;
+  }
+
+  convertBuildStepState(state: string | undefined): {
+    category: string;
+    detail: string;
+  } {
+    if (!state) {
+      return {category: BuildStateCategory.Unknown, detail: 'undefined'};
+    }
+    const detail = state.toLowerCase();
+    //https://docs.microsoft.com/en-us/rest/api/azure/devops/build/timeline/get?view=azure-devops-rest-6.0#taskresult
+    switch (detail) {
+      case 'abandoned':
+      case 'canceled':
+        return {category: BuildStateCategory.Canceled, detail};
+      case 'failed':
+      case 'skipped':
+        return {category: BuildStateCategory.Failed, detail};
+      case 'succeeded':
+        return {category: BuildStateCategory.Success, detail};
+      case 'succeededWithIssues':
+        return {category: BuildStateCategory.Queued, detail};
+      default:
+        return {category: BuildStateCategory.Custom, detail};
+    }
+  }
+
+  // TODO
+  convertBuildStepType(type: string): JobState {
+    if (!type) {
+      return {category: JobCategory.Custom, detail: 'undefined'};
+    }
+    return {category: JobCategory.Custom, detail: type};
+  }
+
+  convertDeploymentStatus(result: string): DeploymentStatus {
+    if (!result) {
+      return {category: DeploymentStatusCategory.Custom, detail: 'undefined'};
+    }
+    const detail = result;
+    switch (result) {
+      case 'canceled':
+        return {category: DeploymentStatusCategory.Canceled, detail};
+      case 'failed':
+        return {category: DeploymentStatusCategory.Failed, detail};
+      case 'succeeded':
+        return {category: DeploymentStatusCategory.Success, detail};
+      default:
+        return {category: DeploymentStatusCategory.Custom, detail};
+    }
+  }
+}
