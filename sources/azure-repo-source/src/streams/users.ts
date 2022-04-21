1e4d126af1acfa2757b0272a8c248ae609990450
Kelvin Tran <huongkstn@gmail.com>
2022-04-22 04:03:24 +0700

    Add missing source

---
+++ sources/azure-repo-source/src/streams/users.ts
@@ -0,0 +1,35 @@
+import {
+  AirbyteLogger,
+  AirbyteStreamBase,
+  StreamKey,
+  SyncMode,
+} from 'faros-airbyte-cdk';
+import {Dictionary} from 'ts-essentials';
+
+import {AzureRepo, AzureRepoConfig} from '../azure-repo';
+import {User} from '../models';
+
+export class Users extends AirbyteStreamBase {
+  constructor(
+    private readonly config: AzureRepoConfig,
+    protected readonly logger: AirbyteLogger
+  ) {
+    super(logger);
+  }
+
+  getJsonSchema(): Dictionary<any, string> {
+    return require('../../resources/schemas/users.json');
+  }
+  get primaryKey(): StreamKey {
+    return 'principalName';
+  }
+
+  async *readRecords(
+    syncMode: SyncMode,
+    cursorField?: string[],
+    streamSlice?: Dictionary<any>
+  ): AsyncGenerator<User> {
+    const azureRepo = await AzureRepo.instance(this.config);
+    yield* azureRepo.getUsers();
+  }
+}
