767b996199643f699122c786e58c76ceaab12213
Yandry Perez Clemente <99700024+ypc-faros@users.noreply.github.com>
2022-05-19 10:55:24 -0400

    feedback and more fixes

--- destinations/airbyte-faros-destination/src/converters/github/workflow_runs.ts
+++ destinations/airbyte-faros-destination/src/converters/github/workflow_runs.ts
@@ -1,5 +1,5 @@
 import {AirbyteRecord} from 'faros-airbyte-cdk';
-import {Utils} from 'faros-feeds-sdk/lib';
+import {Utils} from 'faros-feeds-sdk';
 
 import {DestinationModel, DestinationRecord, StreamContext} from '../converter';
 import {GitHubCommon, GitHubConverter} from './common';
@@ -16,63 +16,51 @@ export class WorkflowRuns extends GitHubConverter {
   ): Promise<ReadonlyArray<DestinationRecord>> {
     const source = this.streamName.source;
     const run = record.record.data;
+    const res: DestinationRecord[] = [];
 
     const repositoryKey = GitHubCommon.parseRepositoryKey(
       run.repository.full_name,
       source
     );
 
+    const pipeline = {
+      uid: run.workflow_id.toString(),
+      organization: repositoryKey.organization,
+    };
+
     const build = {
-      uid: run.id,
+      uid: run.id.toString(),
       name: run.name,
       number: run.run_number,
       createdAt: Utils.toDate(run.created_at),
-      startedAt: Utils.toDate(run.started_at),
-      endedAt: Utils.toDate(run.updated_at),
-      status: this.cicd_BuildStatus(run.conclusion),
+      startedAt: Utils.toDate(run.run_started_at),
+      endedAt: run.conclusion ? Utils.toDate(run.updated_at) : null,
+      status: GitHubCommon.cicd_BuildStatus(run.status, run.conclusion),
       url: run.url,
-      pipeline: {
-        uid: run.workflow_id,
-        organization: repositoryKey.organization,
-      },
+      pipeline,
     };
 
-    const commit = {
-      sha: run.head_commit.id,
-      uid: run.head_commit.id,
-      repository: repositoryKey,
-    };
+    res.push({
+      model: 'cicd_Build',
+      record: build,
+    });
 
-    return [
-      {
-        model: 'cicd_Build',
-        record: build,
-      },
-      {
+    if (run.head_commit) {
+      const commit = {
+        sha: run.head_commit.id,
+        uid: run.head_commit.id,
+        repository: repositoryKey,
+      };
+
+      res.push({
         model: 'cicd_BuildCommitAssociation',
         record: {
-          build,
+          build: {uid: build.uid, pipeline},
           commit,
         },
-      },
-    ];
-  }
-
-  private cicd_BuildStatus(conclusion: string): {
-    category: string;
-    detail: string;
-  } {
-    const conclusionLower = conclusion.toLowerCase();
-
-    switch (conclusionLower) {
-      case 'success':
-        return {category: 'Success', detail: conclusionLower};
-      case 'failure':
-        return {category: 'Failed', detail: conclusionLower};
-      case 'cancelled':
-        return {category: 'Canceled', detail: conclusionLower};
-      default:
-        return {category: 'Custom', detail: conclusionLower};
+      });
     }
+
+    return res;
   }
 }
