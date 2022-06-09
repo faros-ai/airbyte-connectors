4a98eece4aad382d71ff7264909fa10c91936fd6
Will Marks <will@faros.ai>
2022-06-09 11:04:04 -0700

    Service now source ot use REST

--- sources/servicenow-source/src/servicenow/models.ts
+++ sources/servicenow-source/src/servicenow/models.ts
@@ -1,55 +1,42 @@
+export interface IncidentRest {
+  readonly sys_id: string;
+  readonly number: string;
+  readonly short_description: string;
+  readonly severity: string;
+  readonly priority: string;
+  readonly state: string;
+  readonly assigned_to: any;
+  readonly opened_by: any;
+  readonly resolved_at: string;
+  readonly opened_at: string;
+  readonly closed_at: string;
+  readonly sys_updated_on: string;
+  readonly business_service: string;
+  readonly cmdb_ci: any;
+}
+
 export interface Incident {
-  readonly number: {
-    readonly value: string;
-  };
-  readonly short_description: {
-    readonly value: string;
-  };
-  readonly severity: {
-    readonly value: string;
-  };
-  readonly priority: {
-    readonly value: string;
-  };
-  readonly state: {
-    readonly displayValue: string;
-  };
-  readonly assigned_to: {
-    readonly value: string;
-  };
-  readonly opened_by: {
-    readonly value: string;
-  };
-  readonly resolved_at: {
-    readonly value: string;
-  };
-  readonly opened_at: {
-    readonly value: string;
-  };
-  readonly closed_at: {
-    readonly value: string;
-  };
-  readonly sys_updated_on: {
-    readonly value: Date;
-  };
-  readonly cmdb_ci: {
-    readonly displayValue: string;
-  };
+  readonly sys_id: string;
+  readonly number: string;
+  readonly short_description: string;
+  readonly severity: string;
+  readonly priority: string;
+  readonly state: string;
+  readonly assigned_to: string;
+  readonly opened_by: string;
+  readonly resolved_at: string;
+  readonly opened_at: string;
+  readonly closed_at: string;
+  readonly sys_updated_on: string;
+  readonly business_service: string;
+  readonly cmdb_ci: string;
 }
 
 export interface User {
-  readonly name: {
-    readonly value: string;
-  };
-  readonly sys_id: {
-    readonly value: string;
-  };
-  readonly email: {
-    readonly value: string;
-  };
-  readonly sys_updated_on: {
-    readonly value: Date;
-  };
+  readonly name: string;
+  readonly sys_id: string;
+  readonly email: string;
+  readonly sys_updated_on: string;
 }
 
 export interface Pagination {
