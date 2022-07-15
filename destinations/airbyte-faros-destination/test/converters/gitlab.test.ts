2c9a35041234d137ac670880eb706a06683f2424
Thomas Gerber <thomas@faros.ai>
2022-07-15 15:30:17 -0700

    GitLab build createdAt and commit association (#580)
    * GitLab build createdAt and commit association
    * Adds test to validate record schema

--- destinations/airbyte-faros-destination/test/converters/gitlab.test.ts
+++ destinations/airbyte-faros-destination/test/converters/gitlab.test.ts
@@ -30,6 +30,58 @@ describe('gitlab', () => {
     await mockttp.stop();
   });
 
+  test('process and write records', async () => {
+    await mockttp
+      .forPost('/graphs/test-graph/models')
+      .withQuery({schema: 'canonical'})
+      .once()
+      .thenReply(200, JSON.stringify({}));
+
+    await mockttp
+      .forPost('/graphs/test-graph/revisions')
+      .once()
+      .thenReply(
+        200,
+        JSON.stringify({
+          entrySchema: graphSchema,
+          revision: {uid: revisionId, lock: {state: {}}},
+        })
+      );
+
+    let entriesSize = 0;
+    await mockttp
+      .forPost(`/graphs/test-graph/revisions/${revisionId}/entries`)
+      .thenCallback(async (r) => {
+        entriesSize = r.body.buffer.length;
+        return {statusCode: 204};
+      });
+
+    await mockttp
+      .forPatch(`/graphs/test-graph/revisions/${revisionId}`)
+      .withJsonBodyIncluding({status: 'active'})
+      .once()
+      .thenReply(204);
+
+    const cli = await CLI.runWith([
+      'write',
+      '--config',
+      configPath,
+      '--catalog',
+      catalogPath,
+    ]);
+    cli.stdin.end(gitlabAllStreamsLog, 'utf8');
+
+    const stdout = await read(cli.stdout);
+    logger.debug(stdout);
+
+    expect(stdout).toMatch('Wrote 77 records');
+    expect(stdout).toMatch('Errored 0 records');
+    expect(stdout).toMatch('Skipped 0 records');
+    expect(await read(cli.stderr)).toBe('');
+    expect(await cli.wait()).toBe(0);
+    expect(entriesSize).toBeGreaterThan(0);
+  });
+
   test('process records from all streams', async () => {
     const cli = await CLI.runWith([
       'write',
@@ -71,6 +123,7 @@ describe('gitlab', () => {
 
     const writtenByModel = {
       cicd_Build: 2,
+      cicd_BuildCommitAssociation: 2,
       cicd_BuildStep: 8,
       cicd_Organization: 1,
       cicd_Pipeline: 1,
