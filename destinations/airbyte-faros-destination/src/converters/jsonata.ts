cd5bb6cc2226109a61b93e3098366d2c7672eebe
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2023-01-19 11:37:17 -0500

    Improve JSONata converter + bump faros-js-client (#872)

--- destinations/airbyte-faros-destination/src/converters/jsonata.ts
+++ destinations/airbyte-faros-destination/src/converters/jsonata.ts
@@ -1,12 +1,21 @@
+import {ok} from 'assert';
 import {AirbyteRecord} from 'faros-airbyte-cdk';
+import {FarosGraphSchema} from 'faros-js-client';
 import jsonata from 'jsonata';
 import {VError} from 'verror';
 
-import {Converter, DestinationModel, DestinationRecord} from './converter';
+import {
+  Converter,
+  DestinationModel,
+  DestinationRecord,
+  StreamContext,
+} from './converter';
 
 /** Record converter to convert records using provided JSONata expression */
 export class JSONataConverter extends Converter {
   source = 'JSONata';
+
+  private schema: FarosGraphSchema = undefined;
 
   constructor(
     private readonly jsonataExpr: jsonata.Expression,
@@ -20,11 +29,54 @@ export class JSONataConverter extends Converter {
   }
 
   async convert(
-    record: AirbyteRecord
+    record: AirbyteRecord,
+    ctx?: StreamContext
   ): Promise<ReadonlyArray<DestinationRecord>> {
-    const res = this.jsonataExpr.evaluate(record.record);
-    if (!res) return [];
-    if (!Array.isArray(res)) return [res];
+    if (!this.schema && ctx?.farosClient && ctx?.graph) {
+      this.schema = new FarosGraphSchema(
+        await ctx.farosClient.introspect(ctx.graph)
+      );
+    }
+
+    let jsonataResult = this.jsonataExpr.evaluate(record.record);
+    if (!jsonataResult) return [];
+    if (!Array.isArray(jsonataResult)) jsonataResult = [jsonataResult];
+
+    const res = [];
+    for (const result of jsonataResult) {
+      // We expect each jsonata result to conform to:
+      // { '<model>': {<record data>} } or
+      // { '<model>__<operation>': {<record data>} }
+      ok(
+        Object.keys(result).length == 1,
+        'jsonata result should contain a single key'
+      );
+
+      const [model, rec] = Object.entries(result).pop();
+
+      if (this.schema) {
+        if (model.endsWith('__Update')) {
+          for (const key of ['mask', 'patch', 'where']) {
+            this.schema.fixTimestampFields(
+              rec[key],
+              model.replace('__Update', '')
+            );
+          }
+        } else if (model.endsWith('__Deletion')) {
+          this.schema.fixTimestampFields(
+            rec['where'],
+            model.replace('__Deletion', '')
+          );
+        } else if (model.endsWith('__Upsert')) {
+          this.schema.fixTimestampFields(rec, model.replace('__Upsert', ''));
+        } else {
+          this.schema.fixTimestampFields(rec, model);
+        }
+      }
+
+      res.push({model, record: rec});
+    }
+
     return res;
   }
 
---
+++ destinations/airbyte-faros-destination/test/converters/jsonata.test.ts
@@ -0,0 +1,53 @@
+import {AirbyteRecord} from 'faros-airbyte-cdk';
+import fs from 'fs';
+import path from 'path';
+
+import {
+  Converter,
+  ConverterTyped,
+  DestinationRecord,
+  DestinationRecordTyped,
+  StreamName,
+} from '../../src/converters/converter';
+import {JSONataConverter as sut} from '../../src/converters/jsonata';
+
+describe('jsonata', () => {
+  test('fails if empty destination models', async () => {
+    expect(() => sut.make('', [])).toThrow(
+      'Destination models cannot be empty'
+    );
+  });
+
+  test('fails if bad jsonata expression', async () => {
+    expect(() => sut.make('bad jsonata', ['A', 'B'])).toThrow(
+      'Failed to parse JSONata expression'
+    );
+  });
+
+  test('converts records', async () => {
+    expect(
+      await sut
+        .make("(data.({'Person': {'name':name}}))", ['Person'])
+        .convert(AirbyteRecord.make('X', {name: 'John Doe'}))
+    ).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "model": "Person",
+          "record": Object {
+            "name": "John Doe",
+          },
+        },
+      ]
+    `);
+  });
+
+  test('fails if bad jsonata result', async () => {
+    expect(async () => {
+      await sut
+        .make("(data.({'Person': {'name':name}, 'UnexpectedKey':1}))", [
+          'Person',
+        ])
+        .convert(AirbyteRecord.make('X', {name: 'John Doe'}));
+    }).rejects.toThrow('jsonata result should contain a single key');
+  });
+});
