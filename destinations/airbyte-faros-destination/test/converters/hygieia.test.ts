42b25edf1c4a922f3b2b0af631b9687e04a1e915
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2023-02-08 10:00:15 -0500

    Add example hygieia converter (#911)

---
+++ destinations/airbyte-faros-destination/test/converters/hygieia.test.ts
@@ -0,0 +1,83 @@
+import {
+  AirbyteLog,
+  AirbyteLogLevel,
+} from 'faros-airbyte-cdk';
+import _ from 'lodash';
+import {getLocal} from 'mockttp';
+
+import {Edition, InvalidRecordStrategy} from '../../src';
+import {CLI, read} from '../cli';
+import {initMockttp, tempConfig, testLogger} from '../testing-tools';
+import {hygieiaAllStreamsLog} from './data';
+
+describe('hygieia', () => {
+  const logger = testLogger();
+  const mockttp = getLocal({debug: false, recordTraffic: false});
+  const catalogPath = 'test/resources/hygieia/catalog.json';
+  let configPath: string;
+  const streamNamePrefix = 'mytestsource__hygieia__';
+
+  beforeEach(async () => {
+    await initMockttp(mockttp);
+    configPath = await tempConfig(mockttp.url);
+  });
+
+  afterEach(async () => {
+    await mockttp.stop();
+  });
+
+  test('process records from all streams', async () => {
+    const cli = await CLI.runWith([
+      'write',
+      '--config',
+      configPath,
+      '--catalog',
+      catalogPath,
+      '--dry-run',
+    ]);
+    cli.stdin.end(hygieiaAllStreamsLog, 'utf8');
+
+    const stdout = await read(cli.stdout);
+    logger.debug(stdout);
+
+    const processedByStream = {
+      collector_items: 1,
+    };
+    const processed = _(processedByStream)
+      .toPairs()
+      .map((v) => [`${streamNamePrefix}${v[0]}`, v[1]])
+      .orderBy(0, 'asc')
+      .fromPairs()
+      .value();
+
+    const writtenByModel = {
+      cicd_Organization: 1,
+      cicd_Pipeline: 1,
+    };
+
+    const processedTotal = _(processedByStream).values().sum();
+    const writtenTotal = _(writtenByModel).values().sum();
+    expect(stdout).toMatch(`Processed ${processedTotal} records`);
+    expect(stdout).toMatch(`Would write ${writtenTotal} records`);
+    expect(stdout).toMatch('Errored 0 records');
+    expect(stdout).toMatch('Skipped 0 records');
+    expect(stdout).toMatch(
+      JSON.stringify(
+        AirbyteLog.make(
+          AirbyteLogLevel.INFO,
+          `Processed records by stream: ${JSON.stringify(processed)}`
+        )
+      )
+    );
+    expect(stdout).toMatch(
+      JSON.stringify(
+        AirbyteLog.make(
+          AirbyteLogLevel.INFO,
+          `Would write records by model: ${JSON.stringify(writtenByModel)}`
+        )
+      )
+    );
+    expect(await read(cli.stderr)).toBe('');
+    expect(await cli.wait()).toBe(0);
+  });
+});
