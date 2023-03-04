549fb5fb42e68167d5f2b8a74d3f846f0a0e9611
Chris Wu <chris@faros.ai>
2023-03-03 16:48:07 -0800

    Update dest

---
+++ destinations/airbyte-faros-destination/src/converters/statuspage/pages.ts
@@ -0,0 +1,12 @@
+import {AirbyteRecord} from 'faros-airbyte-cdk';
+
+import {DestinationModel, DestinationRecord} from '../converter';
+import {StatuspageConverter} from './common';
+
+export class Pages extends StatuspageConverter {
+  readonly destinationModels: ReadonlyArray<DestinationModel> = [];
+
+  async convert(): Promise<ReadonlyArray<DestinationRecord>> {
+    return [];
+  }
+}
