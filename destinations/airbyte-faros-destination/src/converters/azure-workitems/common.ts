7608288581fe0d3eaa1a18150853d88e92a37379
c.haumann <c.haumann@reply.de>
2023-03-07 17:38:26 +0100

    Changed AzureWorkitems to Azure-Workitems in common.ts

--- destinations/airbyte-faros-destination/src/converters/azure-workitems/common.ts
+++ destinations/airbyte-faros-destination/src/converters/azure-workitems/common.ts
@@ -4,7 +4,7 @@ import {Converter} from '../converter';
 
 /** AzureWorkitems converter base */
 export abstract class AzureWorkitemsConverter extends Converter {
-  source = 'AzureWorkitems';
+  source = 'Azure-Workitems';
   /** Almost every AzureWorkitems record have id property */
   id(record: AirbyteRecord): any {
     return record?.record?.data?.id;
