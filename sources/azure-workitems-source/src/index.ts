8c984b774b6332fa5cd6f8d4a48afeac4d8714fd
Christopher-Haumann <117460514+Christopher-Haumann@users.noreply.github.com>
2023-03-09 18:09:54 +0100

    Added azure-workitems source and converter (#841)

---
+++ sources/azure-workitems-source/src/index.ts
@@ -0,0 +1,47 @@
+import {Command} from 'commander';
+import {
+  AirbyteLogger,
+  AirbyteSourceBase,
+  AirbyteSourceRunner,
+  AirbyteSpec,
+  AirbyteStreamBase,
+} from 'faros-airbyte-cdk';
+import VError from 'verror';
+
+import {AzureWorkitems, AzureWorkitemsConfig} from './azure-workitems';
+import {Boards, Iterations, Users} from './streams';
+import {Workitems} from './streams/workitems';
+
+/** The main entry point. */
+export function mainCommand(): Command {
+  const logger = new AirbyteLogger();
+  const source = new AzureWorkitemsSource(logger);
+  return new AirbyteSourceRunner(logger, source).mainCommand();
+}
+
+/** Example source implementation. */
+export class AzureWorkitemsSource extends AirbyteSourceBase<AzureWorkitemsConfig> {
+  async spec(): Promise<AirbyteSpec> {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    return new AirbyteSpec(require('../resources/spec.json'));
+  }
+  async checkConnection(
+    config: AzureWorkitemsConfig
+  ): Promise<[boolean, VError]> {
+    try {
+      const azureActiveDirectory = await AzureWorkitems.instance(config);
+      await azureActiveDirectory.checkConnection();
+    } catch (err: any) {
+      return [false, err];
+    }
+    return [true, undefined];
+  }
+  streams(config: AzureWorkitemsConfig): AirbyteStreamBase[] {
+    return [
+      new Workitems(config, this.logger),
+      new Users(config, this.logger),
+      new Iterations(config, this.logger),
+      new Boards(config, this.logger),
+    ];
+  }
+}
---
+++ sources/azure-workitems-source/src/streams/iterations.ts
@@ -0,0 +1,26 @@
+import {AirbyteLogger, AirbyteStreamBase, StreamKey} from 'faros-airbyte-cdk';
+import {Dictionary} from 'ts-essentials';
+
+import {AzureWorkitems, AzureWorkitemsConfig} from '../azure-workitems';
+import {Iteration} from '../models';
+
+export class Iterations extends AirbyteStreamBase {
+  constructor(
+    private readonly config: AzureWorkitemsConfig,
+    protected readonly logger: AirbyteLogger
+  ) {
+    super(logger);
+  }
+
+  getJsonSchema(): Dictionary<any, string> {
+    return require('../../resources/schemas/iterations.json');
+  }
+  get primaryKey(): StreamKey {
+    return 'id';
+  }
+
+  async *readRecords(): AsyncGenerator<Iteration> {
+    const azureWorkitem = await AzureWorkitems.instance(this.config);
+    yield* azureWorkitem.getIterations();
+  }
+}
