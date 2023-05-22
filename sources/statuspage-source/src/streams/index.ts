9a13d5cad4cb9c0600a940b5aafde03c052488e5
Chris Wu <chris@faros.ai>
2023-05-22 13:51:50 -0700

    Add components and component_groups streams to Statuspage

--- sources/statuspage-source/src/streams/index.ts
+++ sources/statuspage-source/src/streams/index.ts
@@ -1,5 +1,7 @@
+import {ComponentGroups} from './component_groups';
+import {Components} from './components';
 import {Incidents} from './incidents';
 import {Pages} from './pages';
 import {Users} from './users';
 
-export {Incidents, Pages, Users};
+export {ComponentGroups, Components, Incidents, Pages, Users};
