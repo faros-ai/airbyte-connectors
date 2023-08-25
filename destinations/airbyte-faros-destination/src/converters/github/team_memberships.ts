50715e159b8908802a1f47743885314ec6e88eae
Jesus Rivero <jaorrc@gmail.com>
2023-08-25 11:59:37 -0400

    Feat/add stream teams GitHub (#1112)
    * feat: add support for teams and team memberships streams
    ---------
    
    Co-authored-by: Matthew Tovbin <tovbinm@users.noreply.github.com>
    Co-authored-by: Thomas Gerber <thomas@faros.ai>

---
+++ destinations/airbyte-faros-destination/src/converters/github/team_memberships.ts
@@ -0,0 +1,44 @@
+import {AirbyteRecord} from 'faros-airbyte-cdk';
+import {toLower} from 'lodash';
+
+import {DestinationModel, DestinationRecord} from '../converter';
+import {GitHubConverter} from './common';
+
+export class TeamMemberships extends GitHubConverter {
+  readonly destinationModels: ReadonlyArray<DestinationModel> = [
+    'vcs_TeamMembership',
+    'vcs_User',
+  ];
+
+  async convert(
+    record: AirbyteRecord
+  ): Promise<ReadonlyArray<DestinationRecord>> {
+    const source = this.streamName.source;
+    const membership = record.record.data;
+    const res: DestinationRecord[] = [];
+
+    const team = {
+      uid: toLower(membership.team_slug),
+      source,
+    };
+    const user = {
+      uid: toLower(membership.username),
+      source,
+    };
+
+    res.push({
+      model: 'vcs_User',
+      record: user,
+    });
+
+    res.push({
+      model: 'vcs_TeamMembership',
+      record: {
+        user,
+        team,
+      },
+    });
+
+    return res;
+  }
+}
