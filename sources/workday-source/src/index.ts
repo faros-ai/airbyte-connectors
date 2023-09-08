bb53fe6185b731cb764d6aa3610a2d01184aca21
omreego <omreeg@faros.ai>
2023-09-08 16:25:40 -0700

    Skip conenction check option

--- sources/workday-source/src/index.ts
+++ sources/workday-source/src/index.ts
@@ -26,6 +26,7 @@ export interface WorkdayConfig extends AirbyteConfig {
   readonly tenant: string;
   readonly baseUrl: string;
   readonly credentials: TokenCredentials | UsernamePasswordCredentials;
+  readonly skipConnectionCheck?: boolean;
   readonly limit?: number;
   readonly customReportName?: string;
   readonly timeout?: number;
@@ -45,6 +46,9 @@ export class WorkdaySource extends AirbyteSourceBase<WorkdayConfig> {
     return new AirbyteSpec(require('../resources/spec.json'));
   }
   async checkConnection(config: WorkdayConfig): Promise<[boolean, VError]> {
+    if (config.skipConnectionCheck) {
+      return [true, undefined];
+    }
     try {
       const workday = await Workday.instance(config, this.logger);
       await workday.checkConnection();
