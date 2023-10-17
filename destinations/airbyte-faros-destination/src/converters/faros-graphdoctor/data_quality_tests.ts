85c9fed524647f06f15ef2c8d7831a1948a13621
omreego <omreeg@faros.ai>
2023-10-16 18:29:51 -0700

    graphDoctor converter draft

--- destinations/airbyte-faros-destination/src/converters/faros-graphdoctor/data_quality_tests.ts
+++ destinations/airbyte-faros-destination/src/converters/faros-graphdoctor/data_quality_tests.ts
@@ -8,7 +8,7 @@ import {
   DestinationRecord,
   StreamContext,
 } from '../converter';
-import {DataQualityIssue} from './models';
+import {DataQualityIssue, SummaryKey} from './models';
 
 export class DataQualityTest extends Converter {
   source = 'DataQualityTest';
@@ -17,264 +17,55 @@ export class DataQualityTest extends Converter {
     return record?.record?.data?.id;
   }
   readonly destinationModels: ReadonlyArray<DestinationModel> = [
-    'compute_Application',
-    'ims_Incident',
-    'ims_IncidentApplicationImpact',
-    'ims_IncidentAssignment',
-    'ims_IncidentEvent',
-    'ims_IncidentTag',
-    'ims_IncidentTasks',
-    'ims_Label',
-    'tms_Task',
+    'faros_DataQualityIssue',
+    'faros_DataQualitySummary',
   ];
 
-  private seenTags = new Set<string>();
-  private seenApplications = new Set<string>();
+  private unRecognizedDataIssueObjects: AirbyteRecord[] = [];
 
   async convert(
     record: AirbyteRecord,
     ctx: StreamContext
   ): Promise<ReadonlyArray<DestinationRecord>> {
     const source = this.streamName.source;
-    const incident = record.record.data as Incident;
     const res: DestinationRecord[] = [];
-
-    const maxDescriptionLength = this.maxDescriptionLength(ctx);
-
-    const incidentRef = {uid: incident.id, source};
-    const createdAt = Utils.toDate(incident.created_at);
-    const updatedAt = incident.events
-      ? Utils.toDate(incident.events[incident.events.length - 1].occurred_at)
-      : createdAt;
-
-    let acknowledgedAt: Date = undefined;
-    let resolvedAt: Date = undefined;
-
-    for (const event of incident.events) {
-      const eventType: IncidentEventType = {
-        category: IncidentEventTypeCategory.Created,
-        detail: event.type,
-      };
-
-      const occurredAt = Utils.toDate(event.occurred_at);
-      if (
-        !resolvedAt &&
-        event.data.current_milestone === FirehydrantIncidentMilestone.resolved
-      ) {
-        resolvedAt = occurredAt;
-        eventType.category = IncidentEventTypeCategory.Resolved;
-      }
-      if (
-        !acknowledgedAt &&
-        event.data.current_milestone ===
-          FirehydrantIncidentMilestone.acknowledged
-      ) {
-        acknowledgedAt = occurredAt;
-        eventType.category = IncidentEventTypeCategory.Acknowledged;
-      }
-      res.push({
-        model: 'ims_IncidentEvent',
-        record: {
-          uid: event.id,
-          type: eventType,
-          incident: incidentRef,
-          detail: JSON.stringify(event.data),
-          createdAt: occurredAt,
-        },
-      });
-
-      if (event.data.operation === 'created' && event.data.ticket) {
-        const ticket = event.data.ticket as IncidentTicket;
-        // TODO
-        res.push(
-          this.getTaskDestinationRecord(
-            ticket,
-            source,
-            maxDescriptionLength,
-            occurredAt
-          )
-        );
-        const task = {uid: ticket.id, source};
-        res.push({
-          model: 'ims_IncidentTasks',
-          record: {
-            task,
-            incident: incidentRef,
-          },
-        });
-      }
-    }
-
-    res.push({
-      model: 'ims_Incident',
-      record: {
-        ...incidentRef,
-        title: incident.name,
-        description: incident.description?.substring(0, maxDescriptionLength),
-        url: incident.incident_url,
-        createdAt,
-        updatedAt,
-        acknowledgedAt,
-        resolvedAt,
-        priority: this.getPriority(incident.priority),
-        severity: this.getSeverity(incident.severity),
-        status: this.getIncidentStatus(incident.current_milestone),
-      },
-    });
-
-    if (incident.services) {
-      const applicationMapping = this.applicationMapping(ctx);
-      for (const service of incident.services) {
-        if (!service?.name) continue;
-        const mappedApp = applicationMapping[service.name];
-        const application = Common.computeApplication(
-          mappedApp?.name ?? service.name,
-          mappedApp?.platform
-        );
-        const appKey = application.uid;
-        if (!this.seenApplications.has(appKey)) {
-          res.push({model: 'compute_Application', record: application});
-          this.seenApplications.add(appKey);
-        }
-        res.push({
-          model: 'ims_IncidentApplicationImpact',
-          record: {incident: incidentRef, application},
-        });
-      }
-    }
-
-    for (const assignment of incident.role_assignments) {
-      const assignee = {uid: assignment.user.id, source};
-      res.push({
-        model: 'ims_IncidentAssignment',
-        record: {
-          assignee,
-          incident: incidentRef,
-        },
-      });
-    }
-
-    for (const tag of incident.tag_list) {
-      if (!this.seenTags.has(tag)) {
-        this.seenTags.add(tag);
-        res.push({
-          model: 'ims_Label',
-          record: {
-            name: tag,
-          },
-        });
-      }
-      res.push({
-        model: 'ims_IncidentTag',
-        record: {
-          label: {name: tag},
-          incident: incidentRef,
-        },
-      });
+    const data_obj = record.record.data;
+    if ('faros_DataQualityIssue' in data_obj) {
+      const data_quality_issue: DataQualityIssue = data_obj as DataQualityIssue;
+      return this.getDataQualityIssue(data_quality_issue);
+    } else {
+      this.unRecognizedDataIssueObjects.push(record);
     }
     return res;
   }
 
-  private getTaskDestinationRecord(
-    ticket: IncidentTicket,
-    source: string,
-    maxDescriptionLength: number,
-    occurredAt: Date
-  ): DestinationRecord {
-    return {
-      model: 'tms_Task',
-      record: {
-        uid: ticket.id,
-        name: ticket.summary,
-        description: ticket.description?.substring(0, maxDescriptionLength),
-        source,
-        url: undefined,
-        type: {
-          detail: ticket.type,
-          category: 'Task',
+  private getDataQualityIssue(issue: DataQualityIssue): DestinationRecord[] {
+    const summary_obj = this.getSummaryObjectFromDataIssue(issue);
+    return [
+      {
+        model: 'faros_DataQualityIssue',
+        record: {
+          uid: issue.uid,
+          model: issue.model,
+          description: issue.description,
+          recordIds: issue.recordIds,
+          createdAt: issue.created_at,
+          summary: summary_obj,
         },
-        priority: undefined,
-        status: this.getTaskStatus(ticket.state),
-        points: 0,
-        additionalFields: [],
-        createdAt: occurredAt,
-        updatedAt: occurredAt,
-        statusChangedAt: undefined,
-        statusChangelog: undefined,
-        parent: undefined,
-        creator: {uid: ticket.created_by.id, source},
-        epic: undefined,
-        sprint: undefined,
       },
-    };
-  }
-  private getPriority(priority: string): IncidentPriority {
-    const detail: string = priority;
-    switch (priority) {
-      case FirehydrantIncidentPriority.P1:
-        return {category: IncidentPriorityCategory.Critical, detail};
-      case FirehydrantIncidentPriority.P2:
-        return {category: IncidentPriorityCategory.High, detail};
-      case FirehydrantIncidentPriority.P3:
-        return {category: IncidentPriorityCategory.Medium, detail};
-      case FirehydrantIncidentPriority.P4:
-        return {category: IncidentPriorityCategory.Low, detail};
-      default:
-        return {category: IncidentPriorityCategory.Custom, detail};
-    }
+    ];
   }
 
-  private getSeverity(severity: string): IncidentSeverity {
-    const detail: string = severity;
-    switch (severity) {
-      case FirehydrantIncidentSeverity.SEV1:
-        return {category: IncidentSeverityCategory.Sev1, detail};
-      case FirehydrantIncidentSeverity.SEV2:
-        return {category: IncidentSeverityCategory.Sev2, detail};
-      case FirehydrantIncidentSeverity.SEV3:
-        return {category: IncidentSeverityCategory.Sev3, detail};
-      case FirehydrantIncidentSeverity.SEV4:
-        return {category: IncidentSeverityCategory.Sev4, detail};
-      case FirehydrantIncidentSeverity.SEV5:
-        return {category: IncidentSeverityCategory.Sev5, detail};
-      default:
-        return {category: IncidentSeverityCategory.Custom, detail};
+  private getSummaryObjectFromDataIssue(
+    issue: DataQualityIssue
+  ): SummaryKey | null {
+    let summary_obj = null;
+    if (issue.summary) {
+      summary_obj = {
+        uid: issue.summary.uid,
+        source: issue.summary.source,
+      };
     }
-  }
-
-  //https://support.firehydrant.io/hc/en-us/articles/4403969187604-Incident-Milestones
-  private getIncidentStatus(milestone: string): {
-    category: string;
-    detail: string;
-  } {
-    const detail = milestone;
-    switch (milestone) {
-      case FirehydrantIncidentMilestone.started:
-        return {category: IncidentStatusCategory.Investigating, detail};
-      case FirehydrantIncidentMilestone.detected:
-        return {category: IncidentStatusCategory.Identified, detail};
-      case FirehydrantIncidentMilestone.acknowledged:
-      case FirehydrantIncidentMilestone.firstaction:
-        return {category: IncidentStatusCategory.Monitoring, detail};
-      case FirehydrantIncidentMilestone.mitigated:
-      case FirehydrantIncidentMilestone.resolved:
-        return {category: IncidentStatusCategory.Resolved, detail};
-      default:
-        return {category: IncidentStatusCategory.Custom, detail};
-    }
-  }
-
-  private getTaskStatus(status: string): TaskStatus {
-    const detail: string = status;
-    switch (status) {
-      case IncidentTicketState.open:
-        return {category: TaskStatusCategory.Todo, detail};
-      case IncidentTicketState.in_progress:
-        return {category: TaskStatusCategory.InProgress, detail};
-      case IncidentTicketState.done:
-        return {category: TaskStatusCategory.Done, detail};
-      default:
-        return {category: TaskStatusCategory.Custom, detail};
-    }
+    return summary_obj;
   }
 }
--- destinations/airbyte-faros-destination/src/converters/faros-graphdoctor/models.ts
+++ destinations/airbyte-faros-destination/src/converters/faros-graphdoctor/models.ts
@@ -1,8 +1,13 @@
+export interface SummaryKey {
+  uid: string;
+  source: string;
+}
+
 export interface DataQualityIssue {
-  summary: string;
-  description: string;
-  created_at: string;
-  updated_at: string;
-  discarded_at?: any;
-  tasks: [any];
+  uid: string;
+  description?: string;
+  created_at?: string;
+  recordIds?: [string];
+  model?: string;
+  summary?: SummaryKey;
 }
