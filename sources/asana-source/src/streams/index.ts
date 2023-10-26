52f7884c0a9b208e25916deb32fa8d2ab5dec95e
Yandry Perez Clemente <99700024+ypc-faros@users.noreply.github.com>
2023-10-26 14:30:20 -0400

    Remove sections stream

--- sources/asana-source/src/streams/index.ts
+++ sources/asana-source/src/streams/index.ts
@@ -1,8 +1,7 @@
 import {Projects} from './projects';
-import {Sections} from './sections';
 import {Tags} from './tags';
 import {Tasks} from './tasks';
 import {Users} from './users';
 import {Workspaces} from './workspaces';
 
-export {Projects, Sections, Tags, Tasks, Users, Workspaces};
+export {Projects, Tags, Tasks, Users, Workspaces};
