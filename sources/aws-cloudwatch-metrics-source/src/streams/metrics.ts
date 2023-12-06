990d916c7e44d9bcc3d541cee9ffcc6363fe1c1e
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2023-12-06 09:18:14 -0500

    AWS CloudWatch Metrics source (#1250)

---
+++ sources/aws-cloudwatch-metrics-source/src/streams/metrics.ts
@@ -0,0 +1,104 @@
+import {createHash} from 'crypto';
+import {
+  AirbyteLogger,
+  AirbyteStreamBase,
+  StreamKey,
+  SyncMode,
+} from 'faros-airbyte-cdk';
+import _ from 'lodash';
+import {Dictionary} from 'ts-essentials';
+
+import {
+  CloudWatch,
+  Config,
+  DataPoint,
+  MIN_DATE,
+  NamedQuery,
+} from '../cloudwatch';
+
+const DEFAULT_STREAM_NAME = 'metrics';
+
+type StreamState = {
+  [queryName: string]: {[queryHash: string]: {timestamp: string}};
+};
+type StreamSlice = {
+  query: NamedQuery;
+  queryHash: string;
+};
+
+export class Metrics extends AirbyteStreamBase {
+  private state: StreamState;
+
+  constructor(
+    private readonly config: Config,
+    protected readonly logger: AirbyteLogger
+  ) {
+    super(logger);
+  }
+
+  get name(): string {
+    return this.config.stream_name ?? DEFAULT_STREAM_NAME;
+  }
+
+  getJsonSchema(): Dictionary<any, string> {
+    return require('../../resources/schemas/metrics.json');
+  }
+
+  get primaryKey(): StreamKey {
+    return undefined;
+  }
+
+  get cursorField(): string | string[] {
+    return ['queryName', 'timestamp'];
+  }
+
+  async *streamSlices(): AsyncGenerator<StreamSlice> {
+    for (const query of this.config.queries) {
+      const queryHash = createHash('md5').update(query.query).digest('hex');
+      yield {query, queryHash};
+    }
+  }
+
+  async *readRecords(
+    syncMode: SyncMode,
+    cursorField?: string[],
+    streamSlice?: StreamSlice,
+    streamState?: StreamState
+  ): AsyncGenerator<{queryName: string} & DataPoint> {
+    this.state = syncMode === SyncMode.INCREMENTAL ? streamState ?? {} : {};
+
+    const timestamp = _.get(this.state, [
+      streamSlice?.query.name,
+      streamSlice?.queryHash,
+      'timestamp',
+    ]);
+    const cloudWatch = CloudWatch.instance(this.config);
+
+    if (!_.has(this.state, [streamSlice?.query.name, streamSlice?.queryHash])) {
+      _.set(this.state, [streamSlice?.query.name, streamSlice?.queryHash], {
+        timestamp: MIN_DATE,
+      });
+    }
+
+    for await (const record of cloudWatch.getMetricData(
+      streamSlice?.query.query,
+      timestamp,
+      this.logger
+    )) {
+      const queryTimestamp =
+        this.state?.[streamSlice?.query.name][streamSlice?.queryHash]
+          ?.timestamp;
+      const latestRecordTimestamp = record?.timestamp ?? MIN_DATE;
+      if (new Date(latestRecordTimestamp) > new Date(queryTimestamp)) {
+        this.state[streamSlice?.query.name][streamSlice?.queryHash] = {
+          timestamp: latestRecordTimestamp,
+        };
+      }
+      yield {queryName: streamSlice?.query.name, ...record};
+    }
+  }
+
+  getUpdatedState(): StreamState {
+    return this.state;
+  }
+}
---
+++ sources/aws-cloudwatch-metrics-source/test/index.test.ts
@@ -0,0 +1,199 @@
+import {
+  AirbyteLogger,
+  AirbyteLogLevel,
+  AirbyteSpec,
+  SyncMode,
+} from 'faros-airbyte-cdk';
+import fs from 'fs-extra';
+import VError from 'verror';
+
+import {CloudWatch, Config} from '../src/cloudwatch';
+import * as sut from '../src/index';
+
+describe('index', () => {
+  const logger = new AirbyteLogger(
+    // Shush messages in tests, unless in debug
+    process.env.LOG_LEVEL === 'debug'
+      ? AirbyteLogLevel.DEBUG
+      : AirbyteLogLevel.FATAL
+  );
+
+  const validConfig: Config = {
+    aws_region: 'us-west-2',
+    credentials: {
+      aws_access_key_id: 'YOUR_AWS_ACCESS_KEY_ID',
+      aws_secret_access_key: 'YOUR_AWS_SECRET_ACCESS_KEY',
+      aws_session_token: 'YOUR_AWS_SESSION_TOKEN',
+    },
+    queries: [{name: 'ExampleQuery1', query: '{"metric":"CPUUtilization"}'}],
+    page_size: 100,
+  };
+
+  const source = new sut.CloudWatchMetricsSource(logger);
+
+  test('spec', async () => {
+    await expect(source.spec()).resolves.toStrictEqual(
+      new AirbyteSpec(readResourceFile('spec.json'))
+    );
+  });
+
+  test('check connection - invalid', async () => {
+    await expect(
+      source.checkConnection({...validConfig, aws_region: undefined})
+    ).resolves.toStrictEqual([false, new VError('Please specify AWS region')]);
+    await expect(
+      source.checkConnection({...validConfig, credentials: undefined})
+    ).resolves.toStrictEqual([
+      false,
+      new VError('Please specify AWS credentials'),
+    ]);
+    await expect(
+      source.checkConnection({
+        ...validConfig,
+        credentials: {
+          aws_access_key_id: undefined,
+          aws_secret_access_key: 'YOUR_AWS_SECRET_ACCESS_KEY',
+        },
+      })
+    ).resolves.toStrictEqual([
+      false,
+      new VError('Please specify AWS access key ID'),
+    ]);
+    await expect(
+      source.checkConnection({
+        ...validConfig,
+        credentials: {
+          aws_access_key_id: 'YOUR_AWS_ACCESS_KEY_ID',
+          aws_secret_access_key: undefined,
+        },
+      })
+    ).resolves.toStrictEqual([
+      false,
+      new VError('Please specify AWS secret access key'),
+    ]);
+  });
+
+  test('check connection', async () => {
+    CloudWatch.instance = jest.fn().mockImplementation(() => {
+      return new CloudWatch(
+        {
+          send: jest.fn().mockResolvedValue({}),
+        } as any,
+        '2021-01-01',
+        '2021-01-02',
+        100
+      );
+    });
+    await expect(source.checkConnection(validConfig)).resolves.toStrictEqual([
+      true,
+      undefined,
+    ]);
+  });
+
+  test('metrics incremental', async () => {
+    const fnSend = jest.fn();
+    const latestTimestamp = '2021-01-02T00:00:00.000Z';
+    const expectedData = {
+      MetricDataResults: [
+        {
+          Id: 'm1',
+          Label: 'CPUUtilization',
+          Timestamps: [
+            new Date('2021-01-01T00:00:00.000Z'),
+            new Date(latestTimestamp),
+          ],
+          Values: [0.5, 0.6],
+        },
+      ],
+    };
+    CloudWatch.instance = jest.fn().mockImplementation(() => {
+      return new CloudWatch(
+        {
+          send: fnSend.mockResolvedValue(expectedData),
+        } as any,
+        '2021-01-01',
+        '2021-01-02',
+        100
+      );
+    });
+
+    const streams = source.streams(validConfig);
+    const stream = streams[0];
+    const iter = stream.readRecords(
+      SyncMode.INCREMENTAL,
+      undefined,
+      {
+        query: {name: 'ExampleQuery1', query: '{"metric":"CPUUtilization"}'},
+        queryHash: 'hash',
+      },
+      {
+        ExampleQuery1: {
+          hash: {
+            timestamp: '2021-01-01T00:00:00.000Z',
+          },
+        },
+      }
+    );
+
+    const items = [];
+    for await (const item of iter) {
+      items.push(item);
+    }
+
+    expect(fnSend).toHaveBeenCalledTimes(1);
+    expect(items).toMatchSnapshot();
+    expect(stream.getUpdatedState(undefined, undefined)).toEqual({
+      ExampleQuery1: {
+        hash: {
+          timestamp: latestTimestamp,
+        },
+      },
+    });
+  });
+
+  test('metrics full', async () => {
+    const fnSend = jest.fn();
+    const expectedData = {
+      MetricDataResults: [
+        {
+          Id: 'm1',
+          Label: 'CPUUtilization',
+          Timestamps: [
+            new Date('2021-01-01T00:00:00.000Z'),
+            new Date('2021-01-02T00:00:00.000Z'),
+          ],
+          Values: [0.5, 0.6],
+        },
+      ],
+    };
+    CloudWatch.instance = jest.fn().mockImplementation(() => {
+      return new CloudWatch(
+        {
+          send: fnSend.mockResolvedValue(expectedData),
+        } as any,
+        '2021-01-01',
+        '2021-01-02',
+        100
+      );
+    });
+
+    const streams = source.streams(validConfig);
+    const stream = streams[0];
+    const iter = stream.readRecords(SyncMode.FULL_REFRESH, undefined, {
+      query: {name: 'ExampleQuery1', query: '{"metric":"CPUUtilization"}'},
+      queryHash: 'hash',
+    });
+
+    const items = [];
+    for await (const item of iter) {
+      items.push(item);
+    }
+
+    expect(fnSend).toHaveBeenCalledTimes(1);
+    expect(items).toMatchSnapshot();
+  });
+});
+
+function readResourceFile(fileName: string): any {
+  return JSON.parse(fs.readFileSync(`resources/${fileName}`, 'utf8'));
+}
