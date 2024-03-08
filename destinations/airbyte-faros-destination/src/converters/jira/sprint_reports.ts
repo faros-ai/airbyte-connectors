65d4620d9de82d7bcd77bda14b2768565969c4b3
matiascoulougian <matias@faros.ai>
2024-03-08 15:37:12 -0300

    Finished source and added converter

---
+++ destinations/airbyte-faros-destination/src/converters/jira/sprint_reports.ts
@@ -0,0 +1,31 @@
+import {AirbyteRecord} from 'faros-airbyte-cdk';
+
+import {DestinationModel, DestinationRecord, StreamContext} from '../converter';
+import {JiraConverter} from './common';
+
+export class SprintReports extends JiraConverter {
+  get destinationModels(): ReadonlyArray<DestinationModel> {
+    return ['tms_Sprint'];
+  }
+  async convert(
+    record: AirbyteRecord,
+    ctx: StreamContext
+  ): Promise<ReadonlyArray<DestinationRecord>> {
+    const sprintReport = record.record.data;
+    return [
+      {
+        model: 'tms_Sprint',
+        record: {
+          uid: String(sprintReport.id),
+          completedPoints: sprintReport.completedPoints,
+          completedOutsideSprintPoints:
+            sprintReport.completedInAnotherSprintPoints,
+          notCompletedPoints: sprintReport.notCompletedPoints,
+          removedPoints: sprintReport.puntedPoints,
+          plannedPoints: sprintReport.plannedPoints,
+          source: this.streamName.source,
+        },
+      },
+    ];
+  }
+}
