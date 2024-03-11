5a09102b5bee758fc62e7ff626f6e5c2d309ed0b
matiascoulougian <matias@faros.ai>
2024-03-11 12:51:40 -0300

    task pull requests primary key empty

--- sources/jira-source/src/streams/task_pull_requests.ts
+++ sources/jira-source/src/streams/task_pull_requests.ts
@@ -29,7 +29,7 @@ export class TaskPullRequests extends AirbyteStreamBase {
   }
 
   get primaryKey(): StreamKey | undefined {
-    return 'number';
+    return undefined;
   }
 
   get cursorField(): string | string[] {
