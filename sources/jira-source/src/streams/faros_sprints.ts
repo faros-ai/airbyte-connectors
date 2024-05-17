ca0a68c72763247c283cac09d636510d727c6c77
Chalenge Masekera <5385518+chalenge@users.noreply.github.com>
2024-05-17 12:58:13 -0400

    typo
    Signed-off-by: Chalenge Masekera <5385518+chalenge@users.noreply.github.com>

--- sources/jira-source/src/streams/faros_sprints.ts
+++ sources/jira-source/src/streams/faros_sprints.ts
@@ -45,7 +45,7 @@ export class FarosSprints extends StreamWithBoardSlices {
         endDate: sprint.endDate,
         completeDate: sprint.completeDate,
         activatedDate: sprint['activatedDate'],
-        boardId: toInteger(sprint.originBoardId),
+        boardId: toInteger(boardId),
       };
     }
   }
