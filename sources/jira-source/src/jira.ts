5ed201bcfda9f7a908b8dab08075d7153f427c85
Yandry Perez Clemente <99700024+ypc-faros@users.noreply.github.com>
2024-05-31 12:14:13 -0400

    clean imports

--- sources/jira-source/src/jira.ts
+++ sources/jira-source/src/jira.ts
@@ -19,16 +19,7 @@ import * as fs from 'fs';
 import parseGitUrl from 'git-url-parse';
 import https from 'https';
 import jira, {AgileModels, Version2Models} from 'jira.js';
-import _, {
-  chunk,
-  concat,
-  isEmpty,
-  isNil,
-  pick,
-  toInteger,
-  toLower,
-} from 'lodash';
-import moment from 'moment';
+import {chunk, concat, isEmpty, isNil, pick, toInteger, toLower} from 'lodash';
 import pLimit from 'p-limit';
 import path from 'path';
 import {Memoize} from 'typescript-memoize';
