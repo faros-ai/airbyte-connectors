bfa0dc360df6086a24f9a4c65236514c5ebb4815
Diego Bruno <diego@faros.ai>
2024-07-02 10:51:55 -0300

    FAI-11613 | GitHub team memberships stream (source + converter) (#1561)
    * team memberships stream (source + converter)
    
    * added schema

--- sources/github-source/src/index.ts
+++ sources/github-source/src/index.ts
@@ -12,6 +12,7 @@ import {GitHub} from './github';
 import {FarosCopilotSeats} from './streams/faros_copilot_seats';
 import {FarosCopilotUsage} from './streams/faros_copilot_usage';
 import {FarosOrganizations} from './streams/faros_organizations';
+import {FarosTeamMemberships} from './streams/faros_team_memberships';
 import {FarosTeams} from './streams/faros_teams';
 import {GitHubConfig} from './types';
 
@@ -46,6 +47,7 @@ export class GitHubSource extends AirbyteSourceBase<GitHubConfig> {
       new FarosCopilotUsage(config, this.logger),
       new FarosOrganizations(config, this.logger),
       new FarosTeams(config, this.logger),
+      new FarosTeamMemberships(config, this.logger),
     ];
   }
 }
