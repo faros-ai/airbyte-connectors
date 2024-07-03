804c9bda8bdabc0855ee7dcef8a68817e8df09b6
Diego Bruno <diego@faros.ai>
2024-07-03 09:13:39 -0500

    repositories stream (source + converter)

---
+++ destinations/airbyte-faros-destination/src/converters/github/faros_repositories.ts
@@ -0,0 +1,42 @@
+import {AirbyteRecord} from 'faros-airbyte-cdk';
+import {Repository} from 'faros-airbyte-common/github';
+import {Utils} from 'faros-js-client';
+import {toLower} from 'lodash';
+
+import {DestinationModel, DestinationRecord, StreamContext} from '../converter';
+import {GitHubConverter} from './common';
+
+export class FarosRepositories extends GitHubConverter {
+  readonly destinationModels: ReadonlyArray<DestinationModel> = [
+    'vcs_Repository',
+  ];
+
+  async convert(
+    record: AirbyteRecord,
+    ctx: StreamContext
+  ): Promise<ReadonlyArray<DestinationRecord>> {
+    const repo = record.record.data as Repository;
+    return [
+      {
+        model: 'vcs_Repository',
+        record: {
+          name: toLower(repo.name),
+          organization: {
+            uid: toLower(repo.org),
+            source: this.streamName.source,
+          },
+          fullName: repo.full_name,
+          private: repo.private,
+          description: Utils.cleanAndTruncate(repo.description),
+          language: repo.language ?? null,
+          size: repo.size,
+          mainBranch: repo.default_branch,
+          htmlUrl: repo.html_url,
+          topics: repo.topics?.filter((t) => t),
+          createdAt: Utils.toDate(repo?.created_at),
+          updatedAt: Utils.toDate(repo?.updated_at),
+        },
+      },
+    ];
+  }
+}
---
+++ sources/github-source/src/streams/faros_repositories.ts
@@ -0,0 +1,29 @@
+import {StreamKey, SyncMode} from 'faros-airbyte-cdk';
+import {Repository} from 'faros-airbyte-common/github';
+import {Dictionary} from 'ts-essentials';
+
+import {GitHub} from '../github';
+import {OrgStreamSlice, StreamWithOrgSlices} from './common';
+
+export class FarosRepositories extends StreamWithOrgSlices {
+  getJsonSchema(): Dictionary<any, string> {
+    return require('../../resources/schemas/farosRepositories.json');
+  }
+
+  get primaryKey(): StreamKey {
+    return ['org', 'name'];
+  }
+
+  async *readRecords(
+    syncMode: SyncMode,
+    cursorField?: string[],
+    streamSlice?: OrgStreamSlice
+  ): AsyncGenerator<Repository> {
+    const github = await GitHub.instance(this.config, this.logger);
+    const org = streamSlice?.org;
+    const repositories = await github.getRepositories(org);
+    for (const repository of repositories) {
+      yield repository;
+    }
+  }
+}
