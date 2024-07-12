4253ab1ea86c597255f17b1006a07990f65397f6
Ted Tuttle <ted@faros.ai>
2024-07-12 16:06:30 -0700

    FAI-12219: Add delete session tracking to Faros destination 1. review comments

--- destinations/airbyte-faros-destination/src/common/graphql-client.ts
+++ destinations/airbyte-faros-destination/src/common/graphql-client.ts
@@ -1,5 +1,4 @@
-import {randomUUID} from 'node:crypto';
-
+import {randomUUID} from 'crypto';
 import dateformat from 'date-format';
 import {AirbyteLogger} from 'faros-airbyte-cdk';
 import {paginatedQueryV2, Schema, SchemaLoader} from 'faros-js-client';
