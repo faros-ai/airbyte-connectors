df9d01d6cdb97685476fe07797105fd0056d9b08
matiascoulougian <matias@faros.ai>
2024-08-19 19:09:52 -0300

    Update pipeline stream

--- sources/bitbucket-source/src/streams/pipelines.ts
+++ sources/bitbucket-source/src/streams/pipelines.ts
@@ -1,24 +1,11 @@
-import {
-  AirbyteLogger,
-  AirbyteStreamBase,
-  StreamKey,
-  SyncMode,
-} from 'faros-airbyte-cdk';
+import {StreamKey, SyncMode} from 'faros-airbyte-cdk';
 import {Dictionary} from 'ts-essentials';
 
 import {Bitbucket} from '../bitbucket';
-import {BitbucketConfig, Pipeline} from '../types';
+import {Pipeline} from '../types';
+import {RepoStreamSlice, StreamWithRepoSlices} from './common';
 
-type StreamSlice = {workspace: string; repository: string};
-
-export class Pipelines extends AirbyteStreamBase {
-  constructor(
-    readonly config: BitbucketConfig,
-    readonly logger: AirbyteLogger
-  ) {
-    super(logger);
-  }
-
+export class Pipelines extends StreamWithRepoSlices {
   getJsonSchema(): Dictionary<any, string> {
     return require('../../resources/schemas/pipelines.json');
   }
@@ -26,27 +13,15 @@ export class Pipelines extends AirbyteStreamBase {
     return 'uuid';
   }
 
-  async *streamSlices(): AsyncGenerator<StreamSlice> {
-    const bitbucket = Bitbucket.instance(this.config, this.logger);
-    for (const workspace of this.config.workspaces) {
-      for (const repo of await bitbucket.getRepositories(
-        workspace,
-        this.config.repositories
-      )) {
-        yield {workspace, repository: repo.slug};
-      }
-    }
-  }
-
   async *readRecords(
     syncMode: SyncMode,
     cursorField?: string[],
-    streamSlice?: StreamSlice
+    streamSlice?: RepoStreamSlice
   ): AsyncGenerator<Pipeline> {
     const bitbucket = Bitbucket.instance(this.config, this.logger);
 
     const workspace = streamSlice.workspace;
-    const repoSlug = streamSlice.repository;
+    const repoSlug = streamSlice.repo;
     for (const pipeline of await bitbucket.getPipelines(workspace, repoSlug)) {
       yield pipeline;
     }
