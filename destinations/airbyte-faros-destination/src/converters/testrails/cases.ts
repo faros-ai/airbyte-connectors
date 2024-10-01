f8721187e43f54b7fe8b3de668c9af15bd83bd1e
Alex Shifrin <alex.shifrin@gmail.com>
2024-09-30 18:18:38 -0700

    Add custom fields and populate tag field

--- destinations/airbyte-faros-destination/src/converters/testrails/cases.ts
+++ destinations/airbyte-faros-destination/src/converters/testrails/cases.ts
@@ -22,7 +22,17 @@ export class Cases extends TestRailsConverter {
       testCase.suite_id,
       testCase.id
     );
-    const milestoneTag = `milestone:${testCase.milestone}`;
+    const milestoneTag = typeof testCase.milestone !== 'undefined'
+      ? `milestone:${testCase.milestone}`
+      : null;
+    const automationTypeTag = typeof testCase.custom_automation_type !== 'undefined'
+      ? `automation_type:${testCase.custom_automation_type}`
+      : null;
+    const updateAutomationTag = typeof testCase.custom_update_automation !== 'undefined'
+      ? `update_automation:${testCase.custom_update_automation}`
+      : null;
+    const tags = [milestoneTag, automationTypeTag, updateAutomationTag].filter(tag => tag !== null);
+    const formattedTags = tags.map(tag => `{${tag}}`).join('');
 
     res.push({
       model: 'qa_TestCase',
@@ -30,7 +40,7 @@ export class Cases extends TestRailsConverter {
         uid,
         name: testCase.title,
         source,
-        tags: [milestoneTag],
+        tags: [formattedTags],
         type: this.convertType(testCase.type),
       },
     });
