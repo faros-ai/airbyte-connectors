240d5e01894ab649175b57bc30363dcb612d2009
Chris Wu <chris@faros.ai>
2024-11-22 12:22:23 -0800

    Handle missing user references in Azure commits and PRs

--- destinations/airbyte-faros-destination/src/converters/azure-repos/models.ts
+++ destinations/airbyte-faros-destination/src/converters/azure-repos/models.ts
@@ -138,7 +138,7 @@ export interface PullRequest {
   mergeId: string;
   lastMergeSourceCommit: MergeSourceCommit;
   lastMergeCommit: MergeSourceCommit;
-  reviewers: [PullRequestReviewer];
+  reviewers: PullRequestReviewer[];
   url: string;
   supportsIterations: boolean;
   repository: PullRequestRepository;
