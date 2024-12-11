379d37747527c0dccd0c29fdc3929af6f1934b10
Yandry Perez Clemente <99700024+ypc-faros@users.noreply.github.com>
2024-12-11 13:12:35 -0500

    affiliation

--- sources/github-source/src/github.ts
+++ sources/github-source/src/github.ts
@@ -2085,7 +2085,7 @@ export class GitHubToken extends GitHub {
       this.baseOctokit.repos.listForAuthenticatedUser,
       {
         per_page: this.pageSize,
-        type: 'member',
+        affiliation: 'organization_member',
       }
     );
     for await (const res of reposIter) {
