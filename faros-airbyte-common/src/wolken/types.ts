80194408ff651d155426f61090cd5085ed0496c2
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2025-01-17 10:55:53 -0500

    FAI-13875 wolken source initial setup (#1886)

---
+++ faros-airbyte-common/src/wolken/types.ts
@@ -0,0 +1,11 @@
+export interface User {
+  readonly userId: number;
+  readonly userPsNo: string;
+  readonly userFname: string;
+  readonly userLname: string;
+  readonly userEmail: string;
+  readonly userPhno: string;
+  readonly userMobno: string;
+  readonly activeUser: boolean;
+  readonly hireDate: string;
+}
---
+++ sources/wolken-source/src/index.ts
@@ -0,0 +1,75 @@
+import {Command} from 'commander';
+import {
+  AirbyteConfiguredCatalog,
+  AirbyteSourceBase,
+  AirbyteSourceLogger,
+  AirbyteSourceRunner,
+  AirbyteSpec,
+  AirbyteState,
+  AirbyteStreamBase,
+} from 'faros-airbyte-cdk';
+import VError from 'verror';
+import {
+  calculateDateRange,
+} from 'faros-airbyte-common/common';
+import {Users} from './streams/users';
+import {DEFAULT_CUTOFF_DAYS, Wolken, WolkenConfig} from './wolken';
+
+/** The main entry point. */
+export function mainCommand(): Command {
+  const logger = new AirbyteSourceLogger();
+  const source = new WolkenSource(logger);
+  return new AirbyteSourceRunner(logger, source).mainCommand();
+}
+
+/** Wolken source implementation. */
+export class WolkenSource extends AirbyteSourceBase<WolkenConfig> {
+  get type(): string {
+    return 'wolken';
+  }
+
+  async spec(): Promise<AirbyteSpec> {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    return new AirbyteSpec(require('../resources/spec.json'));
+  }
+  async checkConnection(config: WolkenConfig): Promise<[boolean, VError]> {
+    try {
+      const wolken = Wolken.instance(config, this.logger);
+      await wolken.checkConnection();
+    } catch (err: any) {
+      return [false, err];
+    }
+    return [true, undefined];
+  }
+
+  streams(config: WolkenConfig): AirbyteStreamBase[] {
+    return [new Users(config, this.logger)];
+  }
+
+  async onBeforeRead(
+    config: WolkenConfig,
+    catalog: AirbyteConfiguredCatalog,
+    state?: AirbyteState
+  ): Promise<{
+    config: WolkenConfig;
+    catalog: AirbyteConfiguredCatalog;
+    state?: AirbyteState;
+  }> {
+    const {startDate, endDate} = calculateDateRange({
+      start_date: config.start_date,
+      end_date: config.end_date,
+      cutoff_days: config.cutoff_days ?? DEFAULT_CUTOFF_DAYS,
+      logger: this.logger.info.bind(this.logger),
+    });
+
+    return {
+      config: {
+        ...config,
+        startDate,
+        endDate,
+      } as WolkenConfig,
+      catalog: catalog,
+      state: state,
+    };
+  }
+}
