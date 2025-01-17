00cb788e3031ac1ce9b7862dcdc9308d92f6e455
Yandry Perez Clemente <99700024+ypc-faros@users.noreply.github.com>
2025-01-17 10:28:06 -0500

    FAI-13875 wolken source initial setup

---
+++ sources/wolken-source/src/streams/users.ts
@@ -0,0 +1,28 @@
+import {AirbyteLogger, AirbyteStreamBase, SyncMode} from 'faros-airbyte-cdk';
+import {User} from 'faros-airbyte-common/wolken';
+import {Dictionary} from 'ts-essentials';
+
+import {Wolken, WolkenConfig} from '../wolken';
+
+export class Users extends AirbyteStreamBase {
+  constructor(
+    private readonly config: WolkenConfig,
+    protected readonly logger: AirbyteLogger
+  ) {
+    super(logger);
+  }
+
+  getJsonSchema(): Dictionary<any, string> {
+    return require('../../resources/schemas/users.json');
+  }
+
+  get primaryKey(): string {
+    return 'userId';
+  }
+
+  async *readRecords(
+  ): AsyncGenerator<User> {
+    const wolken = Wolken.instance(this.config, this.logger);
+    yield* wolken.getUsers();
+  }
+}
