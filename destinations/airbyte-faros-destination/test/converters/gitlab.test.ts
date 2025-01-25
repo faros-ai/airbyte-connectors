a95d59a24cf544eafac0bc6e358f5cf18adb5871
Yandry Perez Clemente <99700024+ypc-faros@users.noreply.github.com>
2025-01-25 13:31:55 -0500

    basic suite gl

--- destinations/airbyte-faros-destination/test/converters/gitlab.test.ts
+++ destinations/airbyte-faros-destination/test/converters/gitlab.test.ts
@@ -1,29 +1,5 @@
-import {getLocal} from 'mockttp';
+import {generateBasicTestSuite} from './utils';
 
-import {
-  initMockttp,
-  tempConfig,
-} from '../testing-tools';
-import {destinationWriteTest} from './utils';
-
-describe('gitlab', () => {
-  const mockttp = getLocal({debug: false, recordTraffic: false});
-  let configPath: string;
-
-  beforeEach(async () => {
-    await initMockttp(mockttp);
-    configPath = await tempConfig({api_url: mockttp.url});
-  });
-
-  afterEach(async () => {
-    await mockttp.stop();
-  });
-
-  test('process records from all streams', async () => {
-    await destinationWriteTest({
-      configPath,
-      catalogPath: 'test/resources/gitlab/catalog.json',
-      inputRecordsPath: 'gitlab/all-streams.log',
-    });
-  });
+generateBasicTestSuite({
+  sourceName: 'gitlab'
 });
