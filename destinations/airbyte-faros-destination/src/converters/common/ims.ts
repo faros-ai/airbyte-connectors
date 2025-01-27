7b15e4e002a2a40c26b4177d79f36ecafc981852
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2025-01-27 10:41:45 -0500

    FAI-13875 Wolken - initial incident converter implementation (#1895)

---
+++ destinations/airbyte-faros-destination/src/converters/common/ims.ts
@@ -0,0 +1,26 @@
+/** Incident severity level category. The lower severity the number, the more and urgent impactful the incident. */
+export enum IncidentSeverityCategory {
+  Sev1 = 'Sev1',
+  Sev2 = 'Sev2', 
+  Sev3 = 'Sev3',
+  Sev4 = 'Sev4',
+  Sev5 = 'Sev5',
+  Custom = 'Custom',
+}
+
+export enum IncidentPriorityCategory {
+  Critical = 'Critical',
+  High = 'High',
+  Medium = 'Medium', 
+  Low = 'Low',
+  Custom = 'Custom',
+}
+
+export enum IncidentStatusCategory {
+  Created = 'Created',
+  Identified = 'Identified',
+  Investigating = 'Investigating',
+  Monitoring = 'Monitoring',
+  Resolved = 'Resolved',
+  Custom = 'Custom',
+}
---
+++ destinations/airbyte-faros-destination/src/converters/wolken/incidents.ts
@@ -0,0 +1,101 @@
+import {AirbyteRecord} from 'faros-airbyte-cdk';
+import {Utils} from 'faros-js-client';
+import {isNil} from 'lodash';
+
+import {Converter, DestinationModel, DestinationRecord, StreamContext} from '../converter';
+import {
+  IncidentPriorityCategory,
+  IncidentStatusCategory,
+} from '../common/ims';
+import {Incident} from 'faros-airbyte-common/wolken';
+
+
+export class Incidents extends Converter {
+  source = 'Wolken';
+
+  id(record: AirbyteRecord) {
+    return record?.record?.data?.ticketId;
+  }
+
+  readonly destinationModels: ReadonlyArray<DestinationModel> = [
+    'ims_Incident',
+    'ims_IncidentAssignment',
+  ];
+
+  async convert(
+    record: AirbyteRecord,
+    ctx: StreamContext
+  ): Promise<ReadonlyArray<DestinationRecord>> {
+    const source = this.streamName.source;
+    const incident = record.record.data as Incident;
+
+    const incidentKey = {
+      uid: incident.ticketId,
+      source,
+    };
+
+    const res = [];
+
+    res.push({
+      model: 'ims_Incident',
+      record: {
+        ...incidentKey,
+        title: incident.subject,
+        description: Utils.cleanAndTruncate(incident.description),
+        // TODO: Add severity
+        priority: this.getPriority(incident.priorityName) ?? null,
+        status: this.getStatus(incident.statusName) ?? null,
+        createdAt: Utils.toDate(incident.createdTime) ?? null,
+        updatedAt: Utils.toDate(incident.updatedTimestamp) ?? null,
+        resolvedAt: Utils.toDate(incident.resolutionTimeStamp) ?? null,
+      },
+    });
+
+    const assigneeUid = incident.assignedUserId;
+    if (assigneeUid) {
+      res.push({
+        model: 'ims_IncidentAssignment',
+        record: {
+          incident: incidentKey,
+          assignee: {
+            uid: assigneeUid,
+            source,
+          },
+        },
+      });
+    }
+
+    return res;
+  }
+
+  private getPriority(
+    priorityName?: string
+  ): {category: IncidentPriorityCategory; detail: string} | undefined {
+    if (!isNil(priorityName)) {
+      return Utils.toCategoryDetail(IncidentPriorityCategory, priorityName, {
+        'Critical - P1': IncidentPriorityCategory.Critical,
+        'High - P2': IncidentPriorityCategory.High,
+        'Medium - P3': IncidentPriorityCategory.Medium,
+        'Low - P4': IncidentPriorityCategory.Low
+      });
+    }
+    return undefined;
+  }
+
+  private getStatus(
+    statusName?: string
+  ): {category: IncidentStatusCategory; detail: string} | undefined {
+    if (!isNil(statusName)) {
+      return Utils.toCategoryDetail(IncidentStatusCategory, statusName, {
+        'Closed': IncidentStatusCategory.Resolved,
+        'In Progress': IncidentStatusCategory.Investigating,
+        'Open': IncidentStatusCategory.Created,
+        'Reopen': IncidentStatusCategory.Identified,
+        'Resolved': IncidentStatusCategory.Resolved,
+        'Waiting': IncidentStatusCategory.Identified,
+      });
+    }
+    return undefined;
+  }
+}
+
