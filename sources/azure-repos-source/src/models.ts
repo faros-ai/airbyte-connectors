c8e80d0e22c759f485a5c3fed30d9a6b11bf5241
Chalenge Masekera <chalenge@faros.ai>
2025-03-28 16:19:37 -0400

    FAI-15701: Write out branches for Azure Repos Pull Requests (#1961)

--- sources/azure-repos-source/src/models.ts
+++ sources/azure-repos-source/src/models.ts
@@ -1,5 +1,8 @@
 import {AzureDevOpsConfig} from 'faros-airbyte-common/azure-devops';
 
 export interface AzureReposConfig extends AzureDevOpsConfig {
+  readonly repositories?: ReadonlyArray<string>;
   readonly branch_pattern?: string;
+  readonly fetch_branch_commits?: boolean;
+  readonly fetch_tags?: boolean;
 }
