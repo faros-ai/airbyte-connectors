83e1c1b46223e96ba3bf42ae23b0f37637ae1a77
Diego Bruno <diego@faros.ai>
2025-04-16 20:55:14 -0300

    remove unused imports

--- sources/github-source/src/streams/faros_copilot_seats.ts
+++ sources/github-source/src/streams/faros_copilot_seats.ts
@@ -1,18 +1,9 @@
 import {StreamKey, SyncMode} from 'faros-airbyte-cdk';
-import {
-  CopilotSeat,
-  CopilotSeatsStreamRecord,
-} from 'faros-airbyte-common/github';
-import {Utils} from 'faros-js-client';
+import {CopilotSeatsStreamRecord} from 'faros-airbyte-common/github';
 import {Dictionary} from 'ts-essentials';
 
 import {GitHub} from '../github';
-import {
-  OrgStreamSlice,
-  StreamBase,
-  StreamState,
-  StreamWithOrgSlices,
-} from './common';
+import {OrgStreamSlice, StreamWithOrgSlices} from './common';
 
 export class FarosCopilotSeats extends StreamWithOrgSlices {
   getJsonSchema(): Dictionary<any, string> {
