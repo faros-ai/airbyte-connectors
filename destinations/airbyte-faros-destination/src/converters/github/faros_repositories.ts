71c596cc756f6cd3cdfbbac96564086c987912c9
Diego Bruno <diego@faros.ai>
2025-05-19 15:44:45 -0300

    remove repository tag from destination models

--- destinations/airbyte-faros-destination/src/converters/github/faros_repositories.ts
+++ destinations/airbyte-faros-destination/src/converters/github/faros_repositories.ts
@@ -12,7 +12,6 @@ export class FarosRepositories extends GitHubConverter {
     'tms_TaskBoard',
     'tms_TaskBoardProjectRelationship',
     'vcs_Repository',
-    'vcs_RepositoryTag',
   ];
 
   async convert(
