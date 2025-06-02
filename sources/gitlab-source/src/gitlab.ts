267a1f681a88b651a26508ed93a9be7dc71fd09a
Yandry Perez Clemente <99700024+ypc-faros@users.noreply.github.com>
2025-06-02 17:50:17 -0400

    undo

--- sources/gitlab-source/src/gitlab.ts
+++ sources/gitlab-source/src/gitlab.ts
@@ -82,8 +82,7 @@ export class GitLab {
       this.logger.error('Failed to fetch GitLab version: %s', err.message);
       throw new VError(
         err,
-        'GitLab authentication failed. Please check your API token and permissions: %s',
-        err.message
+        'GitLab authentication failed. Please check your API token and permissions'
       );
     }
   }
