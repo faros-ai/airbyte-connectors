39ad9e8751bb50b0bf25ba54390f90ad84547cd1
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2025-06-16 10:59:58 -0400

    use eslint-config-faros (#2093)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,16 @@
 module.exports = {
   root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'simple-import-sort'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  ignorePatterns: ['/bin/', '/lib/', '/out/'],
+  extends: 'faros',
+  ignorePatterns: ['.eslintrc.js', '/lib/'],
   rules: {
-    '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'warn',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn', {'args': 'after-used'}],
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/no-var-requires': 'warn',
-    // https://github.com/microsoft/TypeScript/issues/18433
-    'no-restricted-globals': [
+    '@typescript-eslint/no-this-alias': 'off',
+    '@typescript-eslint/no-unused-vars': [
       'error',
-      'closed',
-      'event',
-      'fdescribe',
-      'length',
-      'location',
-      'name',
-      'parent',
-      'top',
-    ],
-    'simple-import-sort/imports': 'error',
-  },
-  overrides: [
-    {
-      files: ['test/**/*.ts'],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
       },
-    },
-  ],
+    ],
+    'max-len': ['error', {code: 100, ignoreUrls: true}],
+  },
 };
