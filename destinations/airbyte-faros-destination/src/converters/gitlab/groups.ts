d583d45feb74b817b0e5db438062db121737eebb
Diego Bruno <diego@faros.ai>
2025-08-06 10:59:16 -0300

    remove alias from community converter

--- destinations/airbyte-faros-destination/src/converters/gitlab/groups.ts
+++ destinations/airbyte-faros-destination/src/converters/gitlab/groups.ts
@@ -1,26 +1,63 @@
 import {AirbyteRecord} from 'faros-airbyte-cdk';
-import {FarosGroupOutput} from 'faros-airbyte-common/gitlab';
+import {Utils} from 'faros-js-client';
 
-import {DestinationModel, DestinationRecord, StreamContext} from '../converter';
-import {GitlabConverter} from './common';
-import {FarosGroups} from './faros_groups';
+import {DestinationModel, DestinationRecord} from '../converter';
+import {GitlabCommon, GitlabConverter} from './common';
 
 export class Groups extends GitlabConverter {
-  private readonly alias = new FarosGroups();
-
-  readonly destinationModels: ReadonlyArray<DestinationModel> =
-    this.alias.destinationModels;
+  readonly destinationModels: ReadonlyArray<DestinationModel> = [
+    'cicd_Organization',
+    'vcs_Organization',
+    'tms_TaskBoard',
+  ];
 
   async convert(
-    record: AirbyteRecord,
-    ctx: StreamContext
+    record: AirbyteRecord
   ): Promise<ReadonlyArray<DestinationRecord>> {
-    const group = record.record.data as FarosGroupOutput;
+    const source = this.streamName.source;
+    const group = record.record.data;
+    const res: DestinationRecord[] = [];
 
-    // Create a modified record with string conversion of group.id
-    const modifiedRecord = structuredClone(record);
-    modifiedRecord.record.data.id = String(group.id);
+    // we only create orgs for top level groups
+    // but create a task board for all groups
+    if (group.path == group.full_path) {
+      res.push({
+        model: 'cicd_Organization',
+        record: {
+          uid: group.full_path,
+          description: Utils.cleanAndTruncate(
+            group.description,
+            GitlabCommon.MAX_DESCRIPTION_LENGTH
+          ),
+          name: group.name,
+          url: group.web_url,
+          source,
+        },
+      });
 
-    return this.alias.convert(modifiedRecord, ctx);
+      res.push({
+        model: 'vcs_Organization',
+        record: {
+          uid: group.full_path,
+          name: group.name,
+          htmlUrl: group.web_url,
+          type: {category: 'Group', detail: ''},
+          createdAt: Utils.toDate(group.created_at),
+          source,
+        },
+      });
+    } else {
+      res.push(
+        GitlabCommon.tms_TaskBoard(
+          {
+            uid: group.full_path.slice(group.full_path.indexOf('/') + 1),
+            source,
+          },
+          group.name
+        )
+      );
+    }
+
+    return res;
   }
 }
