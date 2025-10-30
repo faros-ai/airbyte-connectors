16fa818028a29a01dbd87defed2a3202c66e5ba4
Yandry Perez Clemente <ypc@faros.ai>
2025-10-30 15:41:19 -0400

    Remove unused skip_check_connection field from AirbyteConfig
    This field was not being used and has been replaced by check_connection.

--- faros-airbyte-cdk/src/protocol.ts
+++ faros-airbyte-cdk/src/protocol.ts
@@ -55,7 +55,6 @@ export interface AirbyteConfig {
   compress_state?: boolean;
   debug?: boolean;
   faros_source_id?: string;
-  skip_check_connection?: boolean;
   skip_reset_models?: string[];
   [k: string]: any;
 }
