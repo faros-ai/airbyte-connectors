c7fe0fbb3472a8d7185de10d02bec51cd60a18ed
omreego <omreeg@faros.ai>
2025-12-01 10:58:41 -0800

    Sign

--- scripts/publish-connector.sh
+++ scripts/publish-connector.sh
@@ -9,7 +9,7 @@ fi
 connector_path=$1
 connector_version=$2
 
-SCRIPT_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="${SCRIPT_DIR:-$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd)}"
 
 [[ "${connector_path}" != */ ]] && connector_path="${connector_path}/"
 
